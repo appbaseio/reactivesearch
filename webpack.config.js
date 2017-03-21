0f177f2b99a5e382d536b1505513f16657728211
Deepak Grover <deepakgrover333@gmail.com>
2017-03-21 22:56:05 +0530

    Fix build configs and build examples

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const CHOOSE_CONFIG = process.env.CHOOSE_CONFIG;
 
-module.exports = {
+const default_config = {
 	entry: {
 		ecommerce_recipe: "./examples/ecommerce/app.js",
 		airbnb_recipe: "./examples/airbnb/app.js",
@@ -33,3 +34,79 @@ module.exports = {
 	},
 	externals: ['ws']
 };
+
+const examples_config = {
+	entry: {
+		ecommerce_recipe: "./examples/ecommerce/app.js",
+		airbnb_recipe: "./examples/airbnb/app.js",
+		news_recipe: "./examples/news/app.js"
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		publicPath: "/dist/",
+		filename: '[name].bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				loaders: ["style-loader", "css-loader", "sass-loader"]
+			},
+			{
+				test: /node_modules\/JSONStream\/index\.js$/,
+				loaders: ["shebang-loader", "babel-loader"]
+			}
+		],
+		noParse: ['ws']
+	},
+	externals: ['ws']
+};
+
+const lib_config = {
+	entry: {
+		app: "./app/app.js"
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		publicPath: "/dist/",
+		filename: '[name].bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				loaders: ["style-loader", "css-loader", "sass-loader"]
+			},
+			{
+				test: /node_modules\/JSONStream\/index\.js$/,
+				loaders: ["shebang-loader", "babel-loader"]
+			}
+		],
+		noParse: ['ws']
+	},
+	externals: ['ws']
+};
+
+let final_config;
+switch(CHOOSE_CONFIG) {
+	case 'EXAMPLES':
+		final_config = examples_config;
+		break;
+	case 'LIB':
+		final_config = lib_config;
+		break;
+	default:
+		final_config = default_config;
+}
+
+module.exports = final_config;
