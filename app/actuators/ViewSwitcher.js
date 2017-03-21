ea1207431f9b30807f5944d53d12d9fbb4a71a64
Deepak Grover <deepakgrover333@gmail.com>
2017-03-21 12:26:03 +0530

    update ViewSwitcher behavior

--- app/actuators/ViewSwitcher.js
+++ app/actuators/ViewSwitcher.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 
 export default class ViewSwitcher extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			active: this.props.defaultSelected || this.props.data[0].value
+		};
+	}
+
 	componentDidMount() {
 		this.switchView(this.props.defaultSelected);
 	}
@@ -14,6 +22,9 @@ export default class ViewSwitcher extends Component {
 			const el = document.querySelector("." + item.value);
 			if (el) {
 				if (element === item.value) {
+					this.setState({
+						active: item.value
+					});
 					document.querySelector("." + item.value).style.display = "block";
 				} else {
 					document.querySelector("." + item.value).style.display = "none";
@@ -23,9 +34,17 @@ export default class ViewSwitcher extends Component {
 	}
 
 	renderItems() {
-		return this.props.data.map(item => (
-			<div key={item.value} className="rbc-list-item" onClick={() => this.switchView(item.value)}>{item.label}</div>
-		));
+		return this.props.data.map((item) => {
+			let cx = "";
+
+			if (item.value === this.state.active) {
+				cx = "active";
+			}
+
+			return (
+				<div key={item.value} className={`rbc-list-item ${cx}`} onClick={() => this.switchView(item.value)}>{item.label}</div>
+			);
+		});
 	}
 
 	render() {
