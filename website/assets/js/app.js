311fabc6344a868bc0f8befc560371d50fbc0741
corysimmons <cory@launchboxhq.com>
2017-04-12 16:04:31 -0400

    Depend on newer ver of jQuery; remove junk JS

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -2,48 +2,4 @@ $(document).ready(function() {
 	// Masonry effect
 	// http://packery.metafizzy.co/
 	$('.js-packery').packery();
-
-	var prices = {
-		'monthly': {
-			bootstrap: 29,
-			growth: 89,
-			dedicated: 499
-		},
-		'annually': {
-			bootstrap: 19,
-			growth: 59,
-			dedicated: 299
-		}
-	};
-	function handleResponsive() {
-		$('.tab-pane .img-container').css({
-			'height': 'auto'
-		});
-		var tabHeight = $('.tab-pane.fade.in .img-container').height();
-		$('.tab-pane .img-container').css({
-			'height': tabHeight
-		});
-	}
-	handleResponsive();
-	$(window).resize(function() {
-		handleResponsive();
-	})
-	$("#faq li").on('click', function(e) {
-		var parentNode = e.target.parentNode;
-		if ($(parentNode).hasClass("active"))
-			$(parentNode).removeClass("active");
-		else
-			$(parentNode).addClass("active");
-	});
-	$('.toggleButton .btn').on('click', function() {
-		$('.toggleButton .btn').removeClass('active');
-		var mode = $(this).hasClass('monthly') ? 'monthly' : 'annually';
-		changePlan(mode);
-	});
-	function changePlan(mode) {
-		$('.toggleButton .btn.'+mode).addClass('active');
-		$('#bootstrap-card .plan-price').text(prices[mode].bootstrap);
-		$('#growth-card .plan-price').text(prices[mode].growth);
-		$('#dedicated-card .plan-price').text(prices[mode].dedicated);
-	}
 });
