9e02136a31c6643f736295250660ec83f94645b6
corysimmons <cory@launchboxhq.com>
2017-04-12 18:32:30 -0400

    Add product hunt backlink

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
 	// Masonry effect
 	// http://packery.metafizzy.co/
 	$('.js-packery').packery();
+
+	// Offer backlink to ProductHunt users
+	if (location.href.match(/\?ref=producthunt/)) {
+		setTimeout(function() {
+			$('.producthunt-backlink').fadeIn();
+		}, 600);
+	}
 });
