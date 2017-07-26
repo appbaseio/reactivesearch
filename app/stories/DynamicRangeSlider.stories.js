f425314a8d03cc7dae34fbdca6c18308de2f6b56
Deepak Grover <deepakgrover333@gmail.com>
2017-07-27 00:42:20 +0530

    Fix stories

--- app/stories/DynamicRangeSlider.stories.js
+++ app/stories/DynamicRangeSlider.stories.js
@@ -13,35 +13,16 @@ export default class DynamicRangeSliderDefault extends Component {
 		helper.ResponsiveStory();
 	}
 
-	onData(res) {
-		let result = null;
-		if (res) {
-			let combineData = res.currentData;
-			if (res.mode === "historic") {
-				combineData = res.currentData.concat(res.newData);
-			} else if (res.mode === "streaming") {
-				combineData = helper.combineStreamData(res.currentData, res.newData);
-			}
-			if (combineData) {
-				result = combineData.map((markerData) => {
-					const marker = markerData._source;
-					return this.itemMarkup(marker, markerData);
-				});
-			}
-		}
-		return result;
-	}
-
-	itemMarkup(marker, markerData) {
+	onData(markerData) {
+		const marker = markerData._source;
 		return (
 			<a
 				className="full_row single-record single_record_for_clone"
 				href={marker.event ? marker.event.event_url : ""}
 				target="_blank"
-				rel="noopener noreferrer"
 				key={markerData._id}
 			>
-				<div className="text-container full_row" style={{ paddingLeft: "10px" }}>
+				<div className="text-container full_row" style={{paddingLeft: "15px"}}>
 					<div className="text-head text-overflow full_row">
 						<span className="text-head-info text-overflow">
 							{marker.member ? marker.member.member_name : ""} is going to {marker.event ? marker.event.event_name : ""}
@@ -51,9 +32,7 @@ export default class DynamicRangeSliderDefault extends Component {
 					<div className="text-description text-overflow full_row">
 						<ul className="highlight_tags">
 							{
-								marker.group.group_topics.map(tag => (
-									<li key={tag.topic_name}>{tag.topic_name}</li>
-								))
+								marker.group.group_topics.map((tag, i) => (<li key={i}>{tag.topic_name}</li>))
 							}
 						</ul>
 					</div>
