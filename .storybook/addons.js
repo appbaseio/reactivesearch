fea9c1d2aafd364e5da62089bd90c10ee7d6fb24
Deepak Grover <deepakgrover333@gmail.com>
2017-08-17 00:10:20 +0530

    update knobs panel

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,4 @@
-import "storybook-readme/register";
 import "@storybook/addon-knobs/register";
+import "storybook-readme/register";
 import "@storybook/addon-options/register";
 import "./styles.css";
