11fdbe1685f164b95f232379b8a0a85de05b94fb
Divyanshu Maithani <div.blackcat@gmail.com>
2017-09-15 21:24:25 +0530

    Add onQueryChange to RatingsFilter #92

--- app/sensors/RatingsFilter.js
+++ app/sensors/RatingsFilter.js
@@ -22,6 +22,7 @@ export default class RatingsFilter extends Component {
 
 	// Set query information
 	componentWillMount() {
+		this.previousQuery = null;	// initial value for onQueryChange
 		this.setQueryInfo();
 		this.checkDefault(this.props);
 		this.listenFilter();
@@ -69,12 +70,20 @@ export default class RatingsFilter extends Component {
 
 	// set the query type and input data
 	setQueryInfo() {
+		const getQuery = (value) => {
+			const currentQuery = this.props.customQuery ? this.props.customQuery(value) : this.customQuery(value);
+			if (this.props.onQueryChange && JSON.stringify(this.previousQuery) !== JSON.stringify(currentQuery)) {
+				this.props.onQueryChange(this.previousQuery, currentQuery);
+			}
+			this.previousQuery = currentQuery;
+			return currentQuery;
+		};
 		const obj = {
 			key: this.props.componentId,
 			value: {
 				queryType: this.type,
 				inputData: this.props.dataField,
-				customQuery: this.props.customQuery ? this.props.customQuery : this.customQuery,
+				customQuery: getQuery,
 				reactiveId: this.context.reactiveId,
 				showFilter: this.props.showFilter,
 				filterLabel: this.props.filterLabel ? this.props.filterLabel : this.props.componentId,
@@ -205,7 +214,8 @@ RatingsFilter.propTypes = {
 	URLParams: React.PropTypes.bool,
 	showFilter: React.PropTypes.bool,
 	filterLabel: React.PropTypes.string,
-	className: React.PropTypes.string
+	className: React.PropTypes.string,
+	onQueryChange: React.PropTypes.func
 };
 
 // Default props value
@@ -234,5 +244,6 @@ RatingsFilter.types = {
 	URLParams: TYPES.BOOLEAN,
 	showFilter: TYPES.BOOLEAN,
 	filterLabel: TYPES.STRING,
-	className: TYPES.STRING
+	className: TYPES.STRING,
+	onQueryChange: TYPES.FUNCTION
 };
