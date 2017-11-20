6e4e8c55e896b909538f199dfe7c00275e9674bb
Divyanshu Maithani <div.blackcat@gmail.com>
2017-11-20 19:36:33 +0530

    Add DataController story

---
+++ example/storybook/stories/DataControllerStory/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { ScrollView, View, Text } from "react-native";
+
+import {
+	ReactiveBase,
+	DataController,
+	ReactiveList
+} from "@appbaseio/reactivebase-native";
+
+import { onAllData } from "../../helpers";
+
+const TextFieldStory = (props) => (
+	<ReactiveBase
+		app="car-store"
+		credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c"
+		type="cars"
+	>
+		<ScrollView>
+			<DataController
+				componentId="DataControllerComponent"
+				dataField="brand"
+				{...props}
+			>
+				<View
+					style={{
+						flex: 1,
+						alignItems: "center",
+						padding: 10,
+						borderWidth: 1,
+						borderColor: "palevioletred",
+						borderStyle: "dashed",
+						borderRadius: 5
+					}}
+				>
+					<Text
+						style={{
+							fontSize: 20
+						}}
+					>
+						A custom 💪 UI component
+					</Text>
+				</View>
+			</DataController>
+			<ReactiveList
+				dataField="name"
+				componentId="ReactiveList"
+				size={20}
+				from={0}
+				onAllData={onAllData}
+				pagination
+				defaultQuery={() => ({
+					query: {
+						match_all: {}
+					},
+					sort: {
+						price: { order: "asc" }
+					}
+				})}
+				react={{
+					and: ["DataControllerComponent"]
+				}}
+			/>
+		</ScrollView>
+	</ReactiveBase>
+);
+
+export default TextFieldStory;
--- example/storybook/stories/index.js
+++ example/storybook/stories/index.js
@@ -6,7 +6,27 @@ import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 import { text, boolean, number } from "@storybook/addon-knobs";
 
+import DataControllerStory from "./DataControllerStory";
 import TextFieldStory from "./TextFieldStory";
+
+storiesOf("DataController", module)
+	.add("Basic", () => (
+		<DataControllerStory />
+	))
+	.add("with defaultSelected and customQuery", () => (
+		<DataControllerStory
+			defaultSelected={text("defaultSelected", "BMW")}
+			customQuery={(value) => ({
+				bool: {
+					must: {
+						match: {
+							brand: value
+						}
+					}
+				}
+			})}
+		/>
+	));
 
 storiesOf("TextField", module)
 	.add("Basic", () => (
@@ -15,3 +35,6 @@ storiesOf("TextField", module)
 	.add("With placeholder", () => (
 		<TextFieldStory placeholder={text("placeholder", "Search Cars")} />
 	))
+	.add("With defaultSelected", () => (
+		<TextFieldStory defaultSelected={text("defaultSelected", "BMW")} />
+	));
