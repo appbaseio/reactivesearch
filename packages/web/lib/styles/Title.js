d08773d9f33216286f49f89868c3904f5cf603f8
Deepak Grover <deepakgrover333@gmail.com>
2017-11-28 15:06:17 +0530

    remove lib

--- packages/web/lib/styles/Title.js
+++
@@ -1,5 +0,0 @@
-import styled from "react-emotion";
-
-var Title = /*#__PURE__*/styled("h2")("margin:0 0 8px;font-size:1rem;");
-
-export default Title;
--- packages/web/lib/styles/base.js
+++
@@ -1,9 +0,0 @@
-import styled from "react-emotion";
-
-var Base = /*#__PURE__*/styled("div")("font-family:", function (props) {
-	return props.theme.fontFamily;
-}, ";font-size:", function (props) {
-	return props.theme.fontSize;
-}, ";*,*:before,*:after{box-sizing:border-box;}");
-
-export default Base;
