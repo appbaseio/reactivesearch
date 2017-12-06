512b323fa4b7e7911249a7c767dafb93cba1b67e
Divyanshu Maithani <div.blackcat@gmail.com>
2017-12-06 16:29:09 +0530

    :rotating_light: Linting fixes

--- packages/web/demos/producthunt/src/index.js
+++ packages/web/demos/producthunt/src/index.js
@@ -1,86 +1,100 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import {ReactiveBase, DataSearch, MultiList, ResultList, SelectedFilters} from "@appbaseio/reactivesearch";
-import Box from 'react-layout-components';
+import {
+	ReactiveBase,
+	DataSearch,
+	MultiList,
+	ResultList,
+	SelectedFilters
+} from "@appbaseio/reactivesearch";
+import Box from "react-layout-components";
 
 class ProductHuntApp extends Component {
-  onData = (hit) => {
-    const res = hit._source;
-    const topics = res.topics.map(topic => (
-      <span key={topic} className="tag">{topic}</span>
-    ));
-    return {
-      title: res.name,
-      desc: (
-        <div>
-          <p>{res.tagline}</p>
-          {topics}
-          <div className="stats">
-            <span><i className="fa fa-caret-up"></i> {res.upvotes}</span>
-            <span><i className="fa fa-comment"></i> {res.comments_count}</span>
-          </div>
-        </div>
-      )
-    }
-  }
+	onData = hit => {
+		const res = hit._source;
+		const topics = res.topics.map(topic => (
+			<span key={topic} className="tag">
+				{topic}
+			</span>
+		));
+		return {
+			title: res.name,
+			desc: (
+				<div>
+					<p>{res.tagline}</p>
+					{topics}
+					<div className="stats">
+						<span>
+							<i className="fa fa-caret-up" /> {res.upvotes}
+						</span>
+						<span>
+							<i className="fa fa-comment" /> {res.comments_count}
+						</span>
+					</div>
+				</div>
+			)
+		};
+	};
 
-  render() {
-    return(
-      <ReactiveBase
-        app="producthunt"
-        credentials="We5c0D8OP:b3f3b3ee-529c-41b2-b69a-84245c091105"
-        type="post"
-      >
-        {/* header */}
-        <Box width={"100%"}>
-          <SelectedFilters />
-        </Box>
-        <Box width={"100%"} className="mt-20">
-          <DataSearch
-            title="Search"
-            componentId="searchbox"
-            dataField={["name", "tagline"]}
-            placeholder="Discover products..."
-            URLParams={true}
-            defaultSelected="iphone"
-            filterLabel="Search"
-          />
-        </Box>
-        <Box flexDirection={"row"} className="mt-20">
-          <Box width={"30%"}>
-            {/* left sidebar */}
-            <MultiList
-              componentId="categories"
-              dataField="topics.raw"
-              title="Categories"
-              size={50}
-              sortBy={"count"}
-              showCount={true}
-              queryFormat="and"
-              react={{
-                "and": ["searchbox"]
-              }}
-              URLParams={true}
-              showSearch={false}
-              showCheckbox={false}
-              filterLabel="Categories"
-            />
-          </Box>
-          <Box width={"70%"}>
-            {/* right sidebar */}
-            <ResultList
-              componentId="results"
-              react={{
-                "and": ["categories", "searchbox"]
-              }}
-              onData={this.onData}
-              onResultStats={(results, time) => {return `Found ${results} results in ${time} milliseconds`}}
-            />
-          </Box>
-        </Box>
-      </ReactiveBase>
-    )
-  }
+	render() {
+		return (
+			<ReactiveBase
+				app="producthunt"
+				credentials="We5c0D8OP:b3f3b3ee-529c-41b2-b69a-84245c091105"
+				type="post"
+			>
+				{/* header */}
+				<Box width={"100%"}>
+					<SelectedFilters />
+				</Box>
+				<Box width={"100%"} className="mt-20">
+					<DataSearch
+						title="Search"
+						componentId="searchbox"
+						dataField={["name", "tagline"]}
+						placeholder="Discover products..."
+						URLParams={true}
+						defaultSelected="iphone"
+						filterLabel="Search"
+					/>
+				</Box>
+				<Box flexDirection={"row"} className="mt-20">
+					<Box width={"30%"}>
+						{/* left sidebar */}
+						<MultiList
+							componentId="categories"
+							dataField="topics.raw"
+							title="Categories"
+							size={50}
+							sortBy={"count"}
+							showCount={true}
+							queryFormat="and"
+							react={{
+								and: ["searchbox"]
+							}}
+							URLParams={true}
+							showSearch={false}
+							showCheckbox={false}
+							filterLabel="Categories"
+						/>
+					</Box>
+					<Box width={"70%"}>
+						{/* right sidebar */}
+						<ResultList
+							componentId="results"
+							react={{
+								and: ["categories", "searchbox"]
+							}}
+							onData={this.onData}
+							onResultStats={(results, time) => {
+								return `Found ${results} results in ${time} milliseconds`;
+							}}
+						/>
+					</Box>
+				</Box>
+			</ReactiveBase>
+		);
+	}
 }
 
-ReactDOM.render(<ProductHuntApp/>, document.getElementById("app"));
+ReactDOM.render(<ProductHuntApp />, document.getElementById("app"));
--- packages/web/demos/producthunt/webpack.config.js
+++ packages/web/demos/producthunt/webpack.config.js
@@ -1,22 +1,22 @@
 const path = require("path");
 
 module.exports = {
-    context: path.resolve(__dirname, "src"),
-    entry: {
-        main: "./index.js"
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js",
-        publicPath: "dist/"
-    },
-    module: {
-        rules: [
-            {
-                test: /.jsx?$/,
-                loader: "babel-loader",
-                exclude: /node_modules/
-            },
-        ]
-    }
+	context: path.resolve(__dirname, "src"),
+	entry: {
+		main: "./index.js"
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "index.js",
+		publicPath: "dist/"
+	},
+	module: {
+		rules: [
+			{
+				test: /.jsx?$/,
+				loader: "babel-loader",
+				exclude: /node_modules/
+			}
+		]
+	}
 };
