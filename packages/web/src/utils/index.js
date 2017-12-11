5d2a614eeea289a5ab15f34a87f834945700b112
Divyanshu Maithani <div.blackcat@gmail.com>
2017-12-12 05:19:51 +0530

    Add suggestions utils for #137

---
+++ packages/web/src/utils/index.js
@@ -0,0 +1,56 @@
+// flattens a nested array
+const flatten = (arr) => (
+	arr.reduce((flat, toFlatten) =>
+	  flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten), []
+	)
+);
+
+const extractSuggestion = (val) => {
+	switch (typeof val) {
+		case "string":
+			return val.toLowerCase();
+		case "object":
+			if (Array.isArray(val)) {
+				return flatten(val);
+			} else {
+				return null;
+			}
+		default:
+			return val;
+	}
+}
+
+export const getSuggestions = (fields, suggestions, currentValue) => {
+	let suggestionsList = [];
+	let labelsList = [];
+
+	const populateSuggestionsList = (val) => {
+		// check if the suggestion includes the current value and not already included in other suggestions
+		if (val.includes(currentValue) && !labelsList.includes(val)) {
+			const option = {
+				label: val,
+				value: val
+			};
+			labelsList = [...labelsList, val];
+			suggestionsList = [...suggestionsList, option];
+		}
+	}
+
+	suggestions.forEach(item => {
+		fields.forEach(field => {
+			const label = item._source[field];
+			if (label) {
+				const val = extractSuggestion(label);
+				if (val) {
+					if (Array.isArray(val)) {
+						val.forEach(suggestion => populateSuggestionsList(suggestion));
+					} else {
+						populateSuggestionsList(val);
+					}
+				}
+			}
+		});
+	});
+
+	return suggestionsList;
+};
