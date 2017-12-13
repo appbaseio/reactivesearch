4fb74000f7ef2a9bab267a63ef0c0c68271e074e
Divyanshu Maithani <div.blackcat@gmail.com>
2017-12-13 20:40:58 +0530

    :art: Add Content styled component

---
+++ packages/web/src/styles/Content.js
@@ -0,0 +1,12 @@
+import styled, { css } from "react-emotion";
+
+const alert = ({ theme }) => css`
+	color: ${theme.alertColor};
+`;
+
+const Content = styled.div`
+	${props => props.alert && alert};
+	margin: 8px;
+`;
+
+export default Content;
