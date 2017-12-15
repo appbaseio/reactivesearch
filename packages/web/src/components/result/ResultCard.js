1439f4e8e6683a7f80112bbe14b5b2362803b468
Deepak Grover <deepakgrover333@gmail.com>
2017-12-15 23:13:35 +0530

    Remove Fragment (v16.x) usage from Result components

--- packages/web/src/components/result/ResultCard.js
+++ packages/web/src/components/result/ResultCard.js
@@ -17,8 +17,6 @@ import {
 } from "@appbaseio/reactivecore/lib/utils/helper";
 import types from "@appbaseio/reactivecore/lib/utils/types";
 
-import ResultHighlight from "./addons/ResultHighlight";
-
 import Button, { pagination } from "../../styles/Button";
 import Card, { container, Image } from "../../styles/Card";
 
@@ -266,7 +264,16 @@ class ResultCard extends Component {
 		const result = this.props.onData(item);
 		return (<Card key={item._id} href={result.url}>
 			<Image src={result.image}></Image>
-			<ResultHighlight result={result} />
+			{
+				typeof result.title === "string"
+					? <Title dangerouslySetInnerHTML={{ __html: result.title }} />
+					: <Title>{result.title}</Title>
+			}
+			{
+				typeof result.desc === "string"
+					? <article dangerouslySetInnerHTML={{ __html: result.desc }} />
+					: <article>{result.desc}</article>
+			}
 		</Card>);
 	};
 
