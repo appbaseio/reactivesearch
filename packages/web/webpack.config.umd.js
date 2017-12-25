a47b18c8aa8e51cf1db5c40bf620e9c8fc2b18d8
Deepak Grover <deepakgrover333@gmail.com>
2017-12-25 21:41:28 +0530

    Allow flat-bundling in web umd

--- packages/web/webpack.config.umd.js
+++ packages/web/webpack.config.umd.js
@@ -40,6 +40,7 @@ module.exports = {
 		}
 	],
 	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin(),
 		new webpack.DefinePlugin({
 			"process.env.NODE_ENV": JSON.stringify("production")
 		}),
