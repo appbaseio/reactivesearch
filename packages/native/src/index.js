2beb48b46fd87d26478469fa61e36c76dd3b390d
Deepak Grover <deepakgrover333@gmail.com>
2017-12-30 02:03:45 +0530

    native: update and fix DatePicker component logic

--- packages/native/src/index.js
+++ packages/native/src/index.js
@@ -2,28 +2,33 @@ import ReactiveBase from './components/basic/ReactiveBase';
 import TextField from './components/basic/TextField';
 import DataController from './components/basic/DataController';
 
+import DatePicker from './components/date/DatePicker';
+import DateRange from './components/date/DateRange';
+
 import DataSearch from './sensors/DataSearch';
 import SingleDropdownList from './sensors/SingleDropdownList';
 import MultiDropdownList from './sensors/MultiDropdownList';
 import SingleDropdownRange from './sensors/SingleDropdownRange';
 import MultiDropdownRange from './sensors/MultiDropdownRange';
-import DatePicker from './sensors/DatePicker';
-import DateRange from './sensors/DateRange';
 import RangeSlider from './sensors/RangeSlider';
 
 import ReactiveList from './actuators/ReactiveList';
 
 export {
-	ReactiveBase,
-	DataController,
-	TextField,
 	DataSearch,
 	SingleDropdownList,
 	MultiDropdownList,
 	SingleDropdownRange,
 	MultiDropdownRange,
 	RangeSlider,
+	ReactiveList,
+
+	// basic
+	ReactiveBase,
+	DataController,
+	TextField,
+
+	// date
 	DatePicker,
 	DateRange,
-	ReactiveList,
 };
