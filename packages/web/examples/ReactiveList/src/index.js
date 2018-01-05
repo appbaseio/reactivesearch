4f6f0454805d43060895147b411ea169c5ce5304
Divyanshu Maithani <div.blackcat@gmail.com>
2018-01-05 17:07:26 +0530

    web: update goodbooks dataset

--- packages/web/examples/ReactiveList/src/index.js
+++ packages/web/examples/ReactiveList/src/index.js
@@ -13,8 +13,8 @@ class Main extends Component {
 	render() {
 		return (
 			<ReactiveBase
-				app="good-books-live"
-				credentials="sHZWU7AYJ:d1e2922c-035c-429f-bfe4-62aa38b1c395"
+				app="good-books-ds"
+				credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
 			>
 				<div className="row">
 					<div className="col">
