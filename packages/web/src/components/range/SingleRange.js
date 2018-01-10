0ad4d479f5fe29ce8a9403d2854a530dc9dc9427
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-01-10 18:00:40 +0530

    web: SingleRange - onQueryChange prop destructing

--- packages/web/src/components/range/SingleRange.js
+++ packages/web/src/components/range/SingleRange.js
@@ -114,10 +114,7 @@ class SingleRange extends Component {
 	updateQuery = (value, props) => {
 		const query = props.customQuery || this.defaultQuery;
 
-		let onQueryChange = null;
-		if (props.onQueryChange) {
-			onQueryChange = props.onQueryChange;
-		}
+		const { onQueryChange = null } = props;
 
 		props.updateQuery({
 			componentId: props.componentId,
