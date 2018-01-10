295afc1a41847f5e8685f74e57cd6f8a7e04caa3
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-01-10 18:04:12 +0530

    native: DatePicker - onQueryChange prop destructing

--- packages/native/src/components/date/DatePicker.js
+++ packages/native/src/components/date/DatePicker.js
@@ -141,10 +141,9 @@ class DatePicker extends Component {
 
 	updateQuery = (value, props) => {
 		const query = props.customQuery || this.defaultQuery;
-		let onQueryChange = null;
-		if (props.onQueryChange) {
-			onQueryChange = props.onQueryChange;
-		}
+
+		const { onQueryChange = null } = props;
+
 		props.updateQuery({
 			componentId: props.componentId,
 			query: query(value, props),
