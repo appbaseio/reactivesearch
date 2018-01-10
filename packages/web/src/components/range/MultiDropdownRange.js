dc05d20f789993db7e35a6652d55fe7ad1b94aa5
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-01-10 18:00:40 +0530

    web: MultiDropdownRange - onQueryChange prop destructing

--- packages/web/src/components/range/MultiDropdownRange.js
+++ packages/web/src/components/range/MultiDropdownRange.js
@@ -156,10 +156,7 @@ class MultiDropdownRange extends Component {
 	updateQuery = (value, props) => {
 		const query = props.customQuery || this.defaultQuery;
 
-		let onQueryChange = null;
-		if (props.onQueryChange) {
-			onQueryChange = props.onQueryChange;
-		}
+		const { onQueryChange = null } = props;
 
 		props.updateQuery({
 			componentId: props.componentId,
