4aacf39fd9e3acc563cb9ab5d39b956355c652f2
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-01-23 21:22:25 +0530

    native: Examples - DataSearch base setup

--- packages/native/examples/DataSearch/src/App.js
+++ packages/native/examples/DataSearch/src/App.js
@@ -1,7 +1,22 @@
 import Expo from 'expo';
 import React, { Component } from 'react';
-import { View, ScrollView, FlatList } from 'react-native';
-import { Text, Header, Body, Title } from 'native-base';
+import {
+	Image,
+	View,
+	ScrollView,
+	Platform,
+	FlatList,
+	TouchableOpacity,
+} from 'react-native';
+import {
+	Body,
+	Header,
+	Text,
+	Title,
+	Spinner,
+} from 'native-base';
+import { FontAwesome as Icon } from '@expo/vector-icons';
+import { web } from 'react-native-communications';
 
 import {
 	ReactiveBase,
@@ -9,7 +24,94 @@ import {
 	ReactiveList,
 } from '@appbaseio/reactivebase-native';
 
-class Main extends Component {
+import { GOOD_BOOKS as APPBASE_CONFIG } from './../../common/credentials';
+
+import {
+	DEFAULT_COLORS as COLORS,
+	commonStyles as common,
+} from './../../common/helpers';
+
+import styles from './Styles';
+
+const COMPONENT_DEMO = 'DataSearch';
+
+export default class Main extends Component {
+	render() {
+		const {
+			isReady,
+		} = this.state;
+
+		if (!isReady) {
+			return (
+				<View style={common.alignCenter}>
+					{this.renderTopBarSpacer()}
+					<Spinner color={COLORS.primary} />
+				</View>
+			);
+		}
+
+		const header = (
+			<Header style={styles.header}>
+				<Body style={styles.headerBody}>
+					<Title style={styles.headerTitle}>{ COMPONENT_DEMO }</Title>
+				</Body>
+			</Header>
+		);
+
+		const componentMarkup = (
+			<View style={styles.componentContainer}>
+				<DataSearch
+					componentId="SearchText"
+					dataField={[
+						'original_title',
+						'original_title.search',
+						'authors',
+						'authors.search',
+					]}
+				/>
+			</View>
+		);
+
+		return (
+			<ReactiveBase
+				app={APPBASE_CONFIG.app}
+				credentials={APPBASE_CONFIG.credentials}
+				type={APPBASE_CONFIG.type}
+			>
+				{this.renderTopBarSpacer()}
+				{header}
+				{componentMarkup}
+				<ScrollView>
+					<View style={[common.container, common.column]}>
+						<View
+							style={[common.fullWidth, common.alignCenter, styles.results]}
+						>
+							<ReactiveList
+								componentId="ReactiveList"
+								dataField="original_title"
+								size={10}
+								onAllData={this.onAllData}
+								pagination
+								paginationAt="bottom"
+								react={{
+									and: ['showAll', 'SearchText'],
+								}}
+								showResultStats={false}
+								defaultQuery={
+									() => ({
+										query: {
+											match_all: {},
+										},
+									})
+								}
+							/>
+						</View>
+					</View>
+				</ScrollView>
+			</ReactiveBase>
+		);
+	}
+
 	state = {
 		isReady: false,
 	}
@@ -21,101 +123,81 @@ class Main extends Component {
 			Ionicons: require('native-base/Fonts/Ionicons.ttf'), // eslint-disable-line global-require
 		});
 
-		this.setState({ isReady: true });
+		this.setState({
+			isReady: true,
+		});
 	}
 
-	onAllData = (items, streamData, loadMore) => (
-		<FlatList
-			style={{ width: '100%' }}
-			data={items || []}
-			keyExtractor={item => item._id}
-			renderItem={({ item }) => (
-				<View style={{ margin: 5 }}>
-					<Text
-						style={{ flex: 1, fontWeight: 'bold' }}
-					>
-						{this.parseToElement(item.name)}
-					</Text>
-					<Text>{item.brand} - {item.model}</Text>
+	renderBookCard = bookData => (
+		<TouchableOpacity
+			onPress={
+				() => web(`https://google.com/search?q=${bookData.original_title}`)
+			}
+		>
+			<View style={[styles.fullWidth, styles.booksRow]}>
+				<View style={styles.booksRowContainer}>
+					<Image
+						source={{
+							uri: bookData.image,
+						}}
+						style={styles.booksImage}
+					/>
 				</View>
-			)}
-			onEndReachedThreshold={0.5}
-			onEndReached={loadMore}
+				<View style={styles.bookInfoSection}>
+					<Text style={styles.bookTitle}>
+						{ bookData.title }
+					</Text>
+					<Text style={styles.bookAuthorSection}>
+						<Text style={styles.bookAuthor}>
+							{ bookData.authors }
+						</Text>
+					</Text>
+					<Text style={styles.bookPublication}>
+						Pub {bookData.original_publication_year}
+					</Text>
+					<View style={styles.bookStars}>
+						{
+							[...Array(bookData.average_rating_rounded)]
+								.map((e, i) => (
+									<Icon
+										key={i} // eslint-disable-line react/no-array-index-key
+										name="star"
+										size={20}
+										color="gold"
+									/>
+								))
+						}
+						<Text style={styles.bookRatings}>
+							({bookData.average_rating})
+						</Text>
+					</View>
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
+
+	onAllData = items => (
+		<FlatList
+			style={styles.listContainer}
+			data={items || []}
+			keyExtractor={item => item.id}
+			renderItem={
+				({ item }) => this.renderBookCard(item)
+			}
 		/>
 	);
 
-	parseToElement = (str) => {
-		const start = str.indexOf('<em>');
-		const end = str.indexOf('</em>');
-
-		if (start > -1) {
-			const pre = str.substring(0, start);
-			const highlight = str.substring(start + 4, end);
-			const post = str.substring(end + 5, str.length);
-
+	renderTopBarSpacer = () => {
+	// Fix status bar top space in Expo
+		if (typeof Expo !== 'undefined' && Platform.OS === 'android') {
 			return (
-				<Text style={{ flex: 1, fontWeight: 'bold' }}>
-					{pre}
-					<Text style={{ backgroundColor: 'yellow' }}>{highlight}</Text>
-					{this.parseToElement(post)}
-				</Text>
+				<View
+					style={styles.topBarSpacer}
+				/>
 			);
 		}
 
-		return str;
-	}
-
-	render() {
-		if (!this.state.isReady) {
-			return <Text>Loading...</Text>;
-		}
-
-		return (
-			<ReactiveBase
-				app="car-store"
-				credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c"
-				type="cars"
-			>
-				<Header>
-					<Body>
-						<Title>ReactiveSearch Native</Title>
-					</Body>
-				</Header>
-				<ScrollView>
-					<View style={{ padding: 10 }}>
-						<DataSearch
-							componentId="DataSearchComponent"
-							dataField="name"
-							defaultSelected="Nissan"
-						/>
-
-						<ReactiveList
-							dataField="name"
-							componentId="ReactiveList"
-							size={20}
-							onAllData={this.onAllData}
-							pagination
-							stream
-							defaultQuery={() => ({
-								query: {
-									match_all: {},
-								},
-								sort: {
-									price: { order: 'asc' },
-								},
-							})}
-							react={{
-								and: [
-									'DataSearchComponent',
-								],
-							}}
-						/>
-					</View>
-				</ScrollView>
-			</ReactiveBase>
-		);
-	}
+		return null;
+	};
 }
 
-module.exports = Main;
-Expo.registerRootComponent(Main);
---
+++ packages/native/examples/DataSearch/src/Styles.js
@@ -0,0 +1,106 @@
+import {
+	Dimensions,
+	StatusBar,
+	StyleSheet,
+} from 'react-native';
+
+import {
+	DEFAULT_COLORS as COLORS,
+	commonStyles as common,
+} from './../../common/helpers';
+
+const { width: deviceWidth } = Dimensions.get('window');
+
+export default StyleSheet.create({
+	topBarSpacer: {
+		paddingTop: StatusBar.currentHeight,
+		backgroundColor: COLORS.primary,
+	},
+	fullWidth: {
+		width: deviceWidth,
+	},
+	controls: {
+		...common.padding2,
+		paddingTop: 0,
+		backgroundColor: '#3cb371',
+		alignItems: 'stretch',
+	},
+	results: {
+		...common.padding2,
+	},
+	componentContainer: {
+		backgroundColor: '#ECEFF1',
+		marginHorizontal: 8,
+		marginVertical: 8,
+	},
+	headerSeperator: {
+		backgroundColor: COLORS.primary,
+		padding: 8,
+	},
+	header: {
+		backgroundColor: COLORS.primary,
+	},
+	headerIcon: {
+		paddingLeft: 5,
+		paddingRight: 10,
+		paddingTop: 3,
+		color: COLORS.secondary,
+	},
+	headerBody: {
+		flex: 1,
+		flexDirection: 'row',
+	},
+	headerTitle: {
+		color: COLORS.secondary,
+		paddingLeft: 10,
+	},
+	listContainer: {
+		width: '100%',
+		marginTop: -25,
+	},
+	booksRow: {
+		flex: 1,
+		flexDirection: 'row',
+		paddingVertical: 15,
+		borderBottomColor: COLORS.seperator,
+		borderBottomWidth: 0.5,
+	},
+	booksRowContainer: {
+		flex: 1,
+		flexGrow: 1,
+		paddingLeft: 15,
+	},
+	booksImage: {
+		height: null,
+		width: null,
+		flex: 1,
+	},
+	bookInfoSection: {
+		flex: 1,
+		flexGrow: 2,
+		flexWrap: 'wrap',
+		padding: 15,
+	},
+	bookTitle: {
+		fontWeight: '800',
+		fontSize: 18,
+	},
+	bookAuthorSection: {
+		paddingTop: 2,
+	},
+	bookAuthor: {
+		color: 'grey',
+	},
+	bookPublication: {
+		paddingTop: 7,
+	},
+	bookStars: {
+		flex: 1,
+		flexDirection: 'row',
+		paddingTop: 5,
+	},
+	bookRatings: {
+		marginTop: -2,
+		paddingLeft: 5,
+	},
+});
