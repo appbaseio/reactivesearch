12127de5087410fe5cc046a47fb5dc2adbcb018c
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-01-23 21:17:55 +0530

    native: Examples - common stylings

---
+++ packages/native/examples/common/credentials.js
@@ -0,0 +1,11 @@
+export const GOOD_BOOKS = {
+	app: 'good-books-ds',
+	credentials: 'nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d',
+	type: 'good-books-ds',
+};
+
+export const GITXPLORE = {
+	app: 'gitxplore-app',
+	credentials: 'FoNt3qFgh:4130b46e-e4d0-4e19-ba8d-dbf85bcf2eb7',
+	type: 'gitxplore-latest',
+};
