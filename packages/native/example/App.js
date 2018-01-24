f2645672bb4b2fdce1e9837672dd48d2a9e48ed3
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-01-25 01:44:46 +0530

    native: example components setup

--- packages/native/example/App.js
+++ packages/native/example/App.js
@@ -1,60 +1,153 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import { View, ScrollView, FlatList } from 'react-native';
+import { Text, Header, Body, Title } from 'native-base';
+
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+	ReactiveBase,
+	DataSearch,
+	TextField,
+	SingleDropdownList,
+	SingleDropdownRange,
+	RangeSlider,
+	DatePicker,
+	DateRange,
+	ReactiveList,
+} from '@appbaseio/reactivebase-native';
 
-import { ReactiveBase } from '@appbaseio/reactivebase-native'
+export default class App extends Component {
+	onAllData = (items, streamData, loadMore) => (
+		<FlatList
+			style={{ width: '100%' }}
+			data={items || []}
+			keyExtractor={item => item._id}
+			renderItem={({ item }) => (
+				<View style={{ margin: 5 }}>
+					<Text
+						style={{ flex: 1, fontWeight: 'bold' }}
+					>
+						{this.parseToElement(item.name)}
+					</Text>
+					<Text>{item.brand} - {item.model}</Text>
+				</View>
+			)}
+			onEndReachedThreshold={0.5}
+			onEndReached={loadMore}
+		/>
+	);
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+	parseToElement = (str) => {
+		const start = str.indexOf('<em>');
+		const end = str.indexOf('</em>');
 
-export default class App extends Component<{}> {
-  render() {
-		console.log("ReactiveBase", ReactiveBase);
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to ReactiveBase!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+		if (start > -1) {
+			const pre = str.substring(0, start);
+			const highlight = str.substring(start + 4, end);
+			const post = str.substring(end + 5, str.length);
+
+			return (
+				<Text style={{ flex: 1, fontWeight: 'bold' }}>
+					{pre}
+					<Text style={{ backgroundColor: 'yellow' }}>{highlight}</Text>
+					{this.parseToElement(post)}
+				</Text>
+			);
+		}
+
+		return str;
+	}
+
+	render() {
+		return (
+			<ReactiveBase
+				app="car-store"
+				credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c"
+				type="cars"
+			>
+				<Header>
+					<Body>
+						<Title>ReactiveSearch Native</Title>
+					</Body>
+				</Header>
+				<ScrollView>
+					<View style={{ padding: 10 }}>
+						<SingleDropdownList
+							componentId="StComponent"
+							dataField="brand.raw"
+						/>
+						<TextField
+							componentId="TextFieldComponent"
+							dataField="color"
+							placeholder="Search color"
+						/>
+
+						<DatePicker
+							dataField="someField"
+							componentId="DatePicker"
+						/>
+
+						<DateRange
+							dataField="someField"
+							componentId="DateRange"
+							defaultSelected={{
+								start: '2017-04-04',
+								end: '2017-04-10',
+							}}
+						/>
+
+						<SingleDropdownRange
+							componentId="SingleDropdownRange"
+							dataField="price"
+							data={[
+								{ start: 0, end: 100, label: 'Cheap' },
+								{ start: 101, end: 200, label: 'Moderate' },
+								{ start: 201, end: 500, label: 'Pricey' },
+								{ start: 501, end: 1000, label: 'First Date' },
+							]}
+							defaultSelected="Pricey"
+						/>
+
+						<RangeSlider
+							componentId="RangeSlider"
+							dataField="rating"
+							range={{
+								start: 0,
+								end: 5,
+							}}
+						/>
+
+						<DataSearch
+							componentId="DataSearchComponent"
+							dataField="name"
+							defaultSelected="Nissan"
+							react={{
+								and: 'TextFieldComponent',
+							}}
+						/>
+
+						<ReactiveList
+							dataField="name"
+							componentId="ReactiveList"
+							size={20}
+							onAllData={this.onAllData}
+							pagination
+							stream
+							defaultQuery={() => ({
+								query: {
+									match_all: {},
+								},
+								sort: {
+									price: { order: 'asc' },
+								},
+							})}
+							react={{
+								and: [
+									'DataSearchComponent',
+								],
+							}}
+						/>
+					</View>
+				</ScrollView>
+			</ReactiveBase>
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
