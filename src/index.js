f7ebfef8dce6cd80333848dc34aa7329daa7b1f7
Divyanshu Maithani <div.blackcat@gmail.com>
2018-01-24 17:13:40 +0530

    Add native site #202

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'emotion-theming';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import HomePageNative from './pages/HomePageNative';
 
 ReactDOM.render(
 	<ThemeProvider theme={{ fontFamily: 'Rubik', primaryColor: '#FF307A', textLight: '#fefefe' }}>
-		<HomePage />
+		<Router>
+			<Fragment>
+				<Route path="/" component={HomePage} />
+				<Route path="/native" component={HomePageNative} />
+			</Fragment>
+		</Router>
 	</ThemeProvider>,
 	document.getElementById('root'),
 );
