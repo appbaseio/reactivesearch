58caa22c2374fbd1124622c21407a8aba652c54f
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-02-05 20:14:08 +0530

    native: examples - daterange innerStyle

--- packages/native/examples/daterange/src/App.js
+++ packages/native/examples/daterange/src/App.js
@@ -2,6 +2,7 @@ import Expo from 'expo';
 import React from 'react';
 import { DrawerNavigator } from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
+
 import DateRange from './DateRangeView';
 
 const navigationOptionsBuilder = (drawerLabel, iconName) => ({
@@ -111,6 +112,22 @@ const RootDrawer = DrawerNavigator({
 				}}
 				showFilter={false}
 				filterLabel="Date Range"
+				innerStyle={{
+					theme: {
+						backgroundColor: '#f2f2f2',
+						calendarBackground: '#f2f2f2',
+						textSectionTitleColor: '#ffb3ff',
+						selectedDayBackgroundColor: 'purple',
+						selectedDayTextColor: '#f2f2f2',
+						todayTextColor: 'purple',
+						dayTextColor: '#cc00cc',
+						textDisabledColor: '#ffb3ff',
+						dotColor: 'purple',
+						selectedDotColor: '#f2f2f2',
+						arrowColor: 'purple',
+						monthTextColor: 'purple',
+					},
+				}}
 				navigation={navigation}
 			/>
 		),
