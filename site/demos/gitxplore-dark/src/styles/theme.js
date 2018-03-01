144bcda537b893932f86f89719266f85dbd21582
Divyanshu Maithani <div.blackcat@gmail.com>
2018-03-01 20:22:31 +0530

    web: add gitxplore-dark demo

--- site/demos/gitxplore-dark/src/styles/theme.js
+++ site/demos/gitxplore-dark/src/styles/theme.js
@@ -3,10 +3,15 @@ const theme = {
 		fontFamily: 'Raleway, Helvetica, sans-serif',
 	},
 	colors: {
-		primaryColor: '#008000',
-		secondaryColor: 'mediumseagreen',
-		titleColor: 'white',
+		primaryColor: 'dodgerblue',
+		secondaryColor: '#303030',
 		lightGray: '#eff3f6',
+	},
+	component: {
+		padding: 10,
+		borderRadius: 3,
+		background: '#424242',
+		boxShadow: '0 1px 3px rgba(0,0,0,0.12),0 1px 2px rgba(0,0,0,0.24)',
 	},
 };
 
