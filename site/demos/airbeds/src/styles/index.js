e2aa1121c20532d1d45a9405aa95acfca1b68c10
Deepak Grover <deepakgrover333@gmail.com>
2018-03-12 17:07:22 +0530

    demo: add map in airbeds example

--- site/demos/airbeds/src/styles/index.js
+++ site/demos/airbeds/src/styles/index.js
@@ -12,13 +12,13 @@ const nav = css`
 	background-color: #FF3A4E;
 	color: #fff;
 	height: 52px;
-	padding: 0 25px;
+	padding: 0 10px 0 25px;
 
 	@media all and (max-width: 768px) {
 		flex-direction: column;
 		height: 100px;
 	}
-	`;
+`;
 
 const title = css`
 	@import url('https://fonts.googleapis.com/css?family=Pacifico');
@@ -28,7 +28,7 @@ const title = css`
 `;
 
 const search = css`
-	width: calc(100% - 320px);
+	width: calc(100% - 255px);
 	@media all and (max-width: 768px) {
 		width: 100%;
 		padding-bottom: 20px;
@@ -41,10 +41,11 @@ const container = css`
 `;
 
 const leftCol = css`
-	width: 320px;
+	width: 280px;
 	height: 100%;
 	padding: 15px 20px;
 	position: fixed;
+	z-index: 10;
 	left: 0;
 	right: 0;
 	border-right: 1px solid #f0f0f0;
@@ -63,20 +64,29 @@ const leftCol = css`
 `;
 
 const rightCol = css`
-	width: calc(100% - 320px);
-	position: relative;
-	left: 320px;
-	padding: 25px 30px;
-	background-color: #fbfbfb;
+	width: calc(100% - 280px);
+	height: calc(100vh - 52px);
+	left: 280px;
+	position: absolute;
 
 	.list {
 		margin-bottom: 30px;
 	}
 
+	.card-container {
+		width: calc(100% - 480px);
+		background-color: #fbfbfb;
+		padding: 20px 0;
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: row;
+		justify-content: space-around;
+	}
+
 	.card {
-		max-width: none;
-		min-width: 0;
+		width: 44%;
 		height: auto;
+		margin-bottom: 20px;
 		background-color: transparent;
 		border: 0;
 		border-radius: 0;
@@ -87,19 +97,22 @@ const rightCol = css`
 		h2 {
 			padding-bottom: 4px;
 			font-size: 1rem;
-			margin: 0;
+			margin: 10px 0 0;
 		}
 
-		image {
+		.card__image {
+			width: 100%;
+			height: 180px;
 			background-size: cover;
+			background-position: center center;
 		}
 
-		.price {
+		.card__price {
 			width: 70px;
 			height: 44px;
 			background-color: #424242;
 			position: absolute;
-			top: 130px;
+			top: 120px;
 			left: 0;
 			color: #fafafa;
 			font-size: 18px;
@@ -109,7 +122,7 @@ const rightCol = css`
 			letter-spacing: 0.03rem;
 		}
 
-		.info {
+		.card__info {
 			color: #555;
 			font-size: 14px;
 			margin: 0;
@@ -121,6 +134,12 @@ const rightCol = css`
 		text-align: right;
 		color: #666;
 		font-size: 15px;
+	}
+
+	.map-container {
+		width: 480px;
+		position: fixed;
+		right: 0;
 	}
 
 	@media all and (min-width: 1441px) {
