1d4bdf8d84f0363928d74becf5150e6633d3b5a8
Deepak Grover <deepakgrover333@gmail.com>
2018-03-21 19:31:41 +0530

    site: add heatmap layer in weather demo

--- site/demos/weather/src/index.js
+++ site/demos/weather/src/index.js
@@ -18,6 +18,9 @@ const Main = () => (
 			dataField="coord"
 			title="Reactive Maps"
 			defaultZoom={4}
+			innerRef={(ref) => {
+				this.mapRef = ref;
+			}}
 			onData={(res) => {
 				let icon = 'https://i.imgur.com/6gsHmLq.png';
 				if (res.clouds.all > 20 && res.clouds.all < 70) {
@@ -44,6 +47,30 @@ const Main = () => (
 			showSearchAsMove
 			searchAsMove
 			defaultMapStyles="Blue Water"
+			onAllData={(hits, streamHits, loadMore, renderMap) => {
+				if (this.mapRef) {
+					if (this.heatmap) {
+						this.heatmap.getData().clear();
+					}
+
+					const heatMapData = hits.map((markerData) => {
+						const location = markerData.coord;
+						const temp = markerData.main.temp || 0;
+						return ({
+							location: new window.google.maps.LatLng(location.lat, location.lon),
+							weight: temp,
+						});
+					});
+
+					this.heatmap = new window.google.maps.visualization.HeatmapLayer({
+						data: heatMapData,
+						radius: 30,
+						map: this.mapRef.map,
+					});
+				}
+
+				return renderMap();
+			}}
 		/>
 	</ReactiveBase>
 );
