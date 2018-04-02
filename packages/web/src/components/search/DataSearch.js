1e3853ffc1549c15560e259ed5157a174fd2a997
Deepak Grover <deepakgrover333@gmail.com>
2018-04-02 15:09:24 +0530

    Update downshift and fix SSR support in DataSearch

--- packages/web/src/components/search/DataSearch.js
+++ packages/web/src/components/search/DataSearch.js
@@ -433,6 +433,7 @@ class DataSearch extends Component {
 				{
 					this.props.autosuggest
 						? (<Downshift
+							id={`${this.props.componentId}-downshift`}
 							onChange={this.onSuggestionSelected}
 							onOuterClick={this.handleOuterClick}
 							onStateChange={this.handleStateChange}
@@ -446,6 +447,7 @@ class DataSearch extends Component {
 							}) => (
 								<div className={suggestionsContainer}>
 									<Input
+										id={`${this.props.componentId}-input`}
 										showIcon={this.props.showIcon}
 										showClear={this.props.showClear}
 										iconPosition={this.props.iconPosition}
