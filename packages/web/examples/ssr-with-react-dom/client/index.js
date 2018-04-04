140f0e32091e7e6e96ed1a625ced1d9c5d39cf43
Divyanshu Maithani <div.blackcat@gmail.com>
2018-04-04 22:34:28 +0530

    web: add ssr with react-dom

---
+++ packages/web/examples/ssr-with-react-dom/client/index.js
@@ -0,0 +1,46 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from '../common/App';
+import BookCard from '../common/BookCard';
+
+// Grab the state from a global variable injected into the server-generated HTML
+const store = window.__PRELOADED_STATE__;
+
+// settings for the ReactiveBase component
+const settings = {
+	app: 'good-books-ds',
+	credentials: 'nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d',
+};
+
+// props for ReactiveSearch components
+// the same props we used server side to generate initial query
+const singleRangeProps = {
+	componentId: 'BookSensor',
+	dataField: 'average_rating',
+	data: [
+		{ start: 0, end: 3, label: 'Rating < 3' },
+		{ start: 3, end: 4, label: 'Rating 3 to 4' },
+		{ start: 4, end: 5, label: 'Rating > 4' },
+	],
+	defaultSelected: 'Rating 3 to 4',
+};
+
+const reactiveListProps = {
+	componentId: 'SearchResult',
+	dataField: 'original_title.raw',
+	from: 0,
+	size: 10,
+	onData: data => (<BookCard key={data._id} data={data} />),
+	react: {
+		and: ['BookSensor'],
+	},
+};
+
+render(<App
+	store={store}
+	settings={settings}
+	singleRangeProps={singleRangeProps}
+	reactiveListProps={reactiveListProps}
+/>, document.getElementById('root'));
---
+++ packages/web/examples/ssr-with-react-dom/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	devtool: 'inline-source-map',
+	entry: {
+		main: './client/index.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/static/',
+	},
+	module: {
+		rules: [
+			{
+				test: /.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				include: __dirname,
+			},
+			{
+				test: /.css?$/,
+				loader: ['style-loader', 'css-loader'],
+				exclude: /node_modules/,
+			},
+		],
+	},
+};
