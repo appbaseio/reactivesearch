ecd9282c3960d3e86c391c8cdb7fd468d9d90ad2
Deepak Grover <deepakgrover333@gmail.com>
2018-04-09 15:21:01 +0530

    maps: Fix onQueryChange support in map components

--- packages/maps/src/components/basic/GeoDistanceSlider.js
+++ packages/maps/src/components/basic/GeoDistanceSlider.js
@@ -7,6 +7,7 @@ import {
 	removeComponent,
 	watchComponent,
 	updateQuery,
+	setQueryListener,
 } from '@appbaseio/reactivecore/lib/actions';
 import {
 	isEqual,
@@ -49,6 +50,7 @@ class GeoDistanceSlider extends Component {
 		if (props.autoLocation) {
 			this.getUserLocation();
 		}
+		props.setQueryListener(props.componentId, props.onQueryChange, null);
 	}
 
 	componentWillMount() {
@@ -233,7 +235,6 @@ class GeoDistanceSlider extends Component {
 
 	updateQuery = (distance, props = this.props) => {
 		const query = props.customQuery || this.defaultQuery;
-		const { onQueryChange = null } = props;
 
 		let value = null;
 		if (distance && this.state.currentLocation) {
@@ -250,7 +251,6 @@ class GeoDistanceSlider extends Component {
 			value,
 			label: props.filterLabel,
 			showFilter: props.showFilter,
-			onQueryChange,
 			URLParams: props.URLParams,
 		});
 	};
@@ -440,6 +440,7 @@ class GeoDistanceSlider extends Component {
 GeoDistanceSlider.propTypes = {
 	addComponent: types.funcRequired,
 	removeComponent: types.funcRequired,
+	setQueryListener: types.funcRequired,
 	updateQuery: types.funcRequired,
 	watchComponent: types.funcRequired,
 	selectedValue: types.selectedValue,
@@ -506,6 +507,8 @@ const mapDispatchtoProps = dispatch => ({
 	removeComponent: component => dispatch(removeComponent(component)),
 	updateQuery: updateQueryObject => dispatch(updateQuery(updateQueryObject)),
 	watchComponent: (component, react) => dispatch(watchComponent(component, react)),
+	setQueryListener: (component, onQueryChange, beforeQueryChange) =>
+		dispatch(setQueryListener(component, onQueryChange, beforeQueryChange)),
 });
 
 export default connect(
