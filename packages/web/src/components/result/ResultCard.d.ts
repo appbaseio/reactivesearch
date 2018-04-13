533b403ce714af78e221bba7786ea689394a1f44
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-13 23:04:45 +0530

    web: update ts linting

--- packages/web/src/components/result/ResultCard.d.ts
+++ packages/web/src/components/result/ResultCard.d.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { CommonProps } from "../../";
 import * as types from "../../types";
-import { ReactiveListProps } from './ReactiveList';
+import { ReactiveListProps } from "./ReactiveList";
 
 export interface ResultCardProps extends ReactiveListProps {
 	innerClass?: types.style;
