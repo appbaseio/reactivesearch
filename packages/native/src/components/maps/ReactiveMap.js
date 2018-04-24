97eb83d896c931f0b6655561022cbafaa4e01f97
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-24 15:34:33 +0530

    native: add maps component

---
+++ packages/native/src/components/maps/ReactiveMap.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { MapView } from 'expo';
+
+export default class ReactiveMap extends React.Component {
+	state = {}
+	render() {
+		console.log('MapView: ', MapView);
+		return (
+			<View>
+				<Text>Map Component</Text>
+				<MapView
+					style={{ flex: 1 }}
+					initialRegion={{
+						latitude: 23.033863,
+						longitude: 72.585022,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421,
+					}}
+				/>
+			</View>
+		);
+	}
+}
