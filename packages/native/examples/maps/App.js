f1c5a914c7a03cf76b9a1394d40c5568befb5337
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-29 01:34:34 +0530

    native: maps - markers, custom markers, callouts/popovers, themes support, pagination

--- packages/native/examples/maps/App.js
+++ packages/native/examples/maps/App.js
@@ -1,34 +1,83 @@
-import Expo, { MapView } from 'expo';
+import Expo from 'expo';
 import React, { Component } from 'react';
-// import { Text, Header, Body, Title } from 'native-base';
+import { ScrollView, View, Text } from 'react-native'; // eslint-disable-line
 
 import {
-	// ReactiveBase,
+	ReactiveBase,
 	ReactiveMap,
+	SingleDropdownList,
 } from '@appbaseio/reactivesearch-native';
 
 class Main extends Component {
 	state = {
-		// isReady: false,
+		isReady: false,
 	}
 
-	// async componentWillMount() {
-	// 	await Expo.Font.loadAsync({
-	// 		Roboto: require('native-base/Fonts/Roboto.ttf'), // eslint-disable-line global-require
-	// 		Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'), // eslint-disable-line global-require
-	// 		Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'), // eslint-disable-line global-require
-	// 	});
+	async componentWillMount() {
+		await Expo.Font.loadAsync({
+			Roboto: require('native-base/Fonts/Roboto.ttf'), // eslint-disable-line global-require
+			Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'), // eslint-disable-line global-require
+			Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'), // eslint-disable-line global-require
+		});
 
-	// 	this.setState({ isReady: true });
-	// }
+		this.setState({ isReady: true });
+	}
+
+	onData = data => (
+		<Text>{data.title}</Text>
+	)
 
 	render() {
-		// if (!this.state.isReady) {
-		// 	return <Text>Loading...</Text>;
-		// }
+		if (!this.state.isReady) {
+			return <Text>Loading...</Text>;
+		}
 
 		return (
-			<ReactiveMap />
+			<View style={{ flex: 1, marginTop: 25 }}>
+				<ReactiveBase
+					app="earthquake"
+					credentials="OrXIHcgHn:d539c6e7-ed14-4407-8214-c227b0600d8e"
+					type="places"
+				>
+					<View style={{ backgroundColor: 'lightblue', marginBottom: 20 }}>
+						<SingleDropdownList
+							componentId="places"
+							dataField="place.raw"
+							size={10}
+							defaultSelected="Chile"
+						/>
+					</View>
+					<View style={{ flex: 1 }}>
+						<ReactiveMap
+							componentId="map"
+							dataField="location"
+							react={{
+								and: 'places',
+							}}
+							onPopoverClick={item => (
+								<Text>Run before it exceeds {item.mag}</Text>
+							)}
+							// onData={item => ({
+							// 	label: item.mag,
+							// })}
+							// onAllData={(hits, streamHits, loadMore, renderMap, renderPagination) => (
+							// 	<View style={{ flex: 1 }}>
+							// 		{renderMap()}
+							// 		<ScrollView style={{ flex: 1 }}>
+							// 			{hits.map(data => (
+							// 				<Text style={{ padding: 10 }} key={data._id}>
+							// 					{data.place} - {data.mag}
+							// 				</Text>
+							// 			))}
+							// 			{renderPagination()}
+							// 		</ScrollView>
+							// 	</View>
+							// )}
+							// pagination
+						/>
+					</View>
+				</ReactiveBase>
+			</View>
 		);
 	}
 }
