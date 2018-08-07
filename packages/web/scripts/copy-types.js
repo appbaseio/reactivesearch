a19154bc3a74bd1450e199c5bc49365d0f63cfc8
Deepak Grover <deepakgrover333@gmail.com>
2018-08-07 21:32:18 +0530

    web: copy typings to lib #466

--- packages/web/scripts/copy-types.js
+++ packages/web/scripts/copy-types.js
@@ -3,7 +3,7 @@ const fse = require('fs-extra'); // eslint-disable-line
 const glob = require('glob'); // eslint-disable-line
 
 function typescriptCopy(from, to) {
-	const files = glob.sync('**/*.d.ts', { cwd: from });
+	const files = glob.sync('**/*.ts', { cwd: from });
 	const cmds = files.map(file =>
 		fse.copy(path.resolve(from, file), path.resolve(to, file)));
 	return Promise.all(cmds);
