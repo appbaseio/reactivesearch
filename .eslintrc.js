8dd09246fd8850a4f66ad78b548bb677b5e66f61
kuldeep <kuldepsaxena155@gmail.com>
2018-09-03 17:24:02 +0530

    fix():Resolved Issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-	extends: ['airbnb', 'plugin:jest/recommended'],
-	plugins: ['jest'],
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: ['airbnb'],
 	env: {
 		browser: true,
 	},
@@ -8,29 +11,12 @@ module.exports = {
 	rules: {
 		indent: 0,
 		'no-tabs': 0,
-		'eol-last': ['error', 'always'],
 		'no-underscore-dangle': 0,
-		camelcase: 0, // shopify and appbase data includes _
 
 		'react/jsx-indent': 0,
 		'react/jsx-indent-props': 0,
 		'react/jsx-filename-extension': 0,
+		'react/jsx-one-expression-per-line': 0,
 		'react/forbid-prop-types': 0,
-		'react/require-default-props': 0,
-	},
-	settings: {
-		'import/resolver': {
-			'babel-module': {
-				root: ['./'],
-				alias: {
-					components: './components',
-					config: './config',
-					utils: './utils',
-					constants: './constants',
-					styles: './styles',
-					template: './templates/active',
-				},
-			},
-		},
 	},
 };
