425f72b87d275f75953fcae9423d9884e557e69e
kuldeep <kuldepsaxena155@gmail.com>
2018-10-30 23:17:31 +0530

    feat(): Added Selected Filters

--- packages/vue/src/index.js
+++ packages/vue/src/index.js
@@ -4,43 +4,46 @@ import DataSearch from './components/search/DataSearch.jsx';
 import SingleList from './components/list/SingleList.jsx';
 import MultiList from './components/list/MultiList.jsx';
 import ReactiveComponent from './components/basic/ReactiveComponent.jsx';
+import SelectedFilters from './components/basic/SelectedFilters.jsx';
 import SingleRange from './components/range/SingleRange.jsx';
 import version from './components/Version/index';
 
 const components = [
-  ReactiveList,
-  ReactiveBase,
-  DataSearch,
-  SingleList,
-  MultiList,
-  SingleRange,
-  ReactiveComponent,
+	ReactiveList,
+	ReactiveBase,
+	DataSearch,
+	SingleList,
+	MultiList,
+	SingleRange,
+	ReactiveComponent,
+	SelectedFilters,
 ];
 
 const install = function (Vue) {
-  components.map((component) => {
-    Vue.use(component);
-    return null;
-  });
+	components.map((component) => {
+		Vue.use(component);
+		return null;
+	});
 };
 
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
+	install(window.Vue);
 }
 
 export {
-  version,
-  install,
-  ReactiveList,
-  ReactiveBase,
-  DataSearch,
-  SingleList,
-  MultiList,
-  SingleRange,
-  ReactiveComponent,
+	version,
+	install,
+	ReactiveList,
+	ReactiveBase,
+	DataSearch,
+	SingleList,
+	MultiList,
+	SingleRange,
+	ReactiveComponent,
+	SelectedFilters,
 };
 
 export default {
-  version,
-  install,
+	version,
+	install,
 };
