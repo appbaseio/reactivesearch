24b0ed52947fa5a3887994b69868b45ab637fd04
Yash Joshi <jyash97@gmail.com>
2018-12-08 16:41:54 +0530

    site: update Car Store Creds & mappings for demos (#673)

--- packages/web/examples/ReactiveComponent/src/index.js
+++ packages/web/examples/ReactiveComponent/src/index.js
@@ -15,8 +15,8 @@ class Main extends Component {
 	render() {
 		return (
 			<ReactiveBase
-				app="car-store"
-				credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c"
+				app="carstore-dataset"
+				credentials="4HWI27QmA:58c731f7-79ab-4f55-a590-7e15c7e36721"
 			>
 				<div className="row">
 					<div className="col">
@@ -25,9 +25,9 @@ class Main extends Component {
 							componentId="CarSensor"
 							defaultQuery={() => ({
 								aggs: {
-									'brand.raw': {
+									'brand.keyword': {
 										terms: {
-											field: 'brand.raw',
+											field: 'brand.keyword',
 											order: {
 												_count: 'desc',
 											},
@@ -44,7 +44,7 @@ class Main extends Component {
 					<div className="col">
 						<ReactiveList
 							componentId="SearchResult"
-							dataField="name"
+							dataField="model"
 							title="ReactiveList"
 							from={0}
 							size={20}
