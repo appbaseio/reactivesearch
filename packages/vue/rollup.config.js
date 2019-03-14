e4ea1490236c9a047c46c364ebc8fba58475d94e
kuldeep <kuldepsaxena155@gmail.com>
2019-03-14 23:51:29 +0530

    feat(vue): Release v1.0.0-beta.9

--- packages/vue/rollup.config.js
+++ packages/vue/rollup.config.js
@@ -15,79 +15,80 @@ const cjs = format === 'cjs';
 let output;
 
 if (es) {
-  output = {
-    file: 'dist/@appbaseio/reactivesearch-vue.es.js',
-    format: 'es',
-  };
+	output = {
+		file: 'dist/@appbaseio/reactivesearch-vue.es.js',
+		format: 'es',
+	};
 } else if (umd) {
-  if (minify) {
-    output = {
-      file: 'dist/@appbaseio/reactivesearch-vue.umd.min.js',
-      format: 'umd',
-    };
-  } else {
-    output = {
-      file: 'dist/@appbaseio/reactivesearch-vue.umd.js',
-      format: 'umd',
-    };
-  }
+	if (minify) {
+		output = {
+			file: 'dist/@appbaseio/reactivesearch-vue.umd.min.js',
+			format: 'umd',
+		};
+	} else {
+		output = {
+			file: 'dist/@appbaseio/reactivesearch-vue.umd.js',
+			format: 'umd',
+		};
+	}
 } else if (cjs) {
-  output = {
-    file: 'dist/@appbaseio/reactivesearch-vue.cjs.js',
-    format: 'cjs',
-  };
+	output = {
+		file: 'dist/@appbaseio/reactivesearch-vue.cjs.js',
+		format: 'cjs',
+	};
 } else if (format) {
-  throw new Error(`invalid format specified: "${format}".`);
+	throw new Error(`invalid format specified: "${format}".`);
 } else {
-  throw new Error('no format specified. --environment FORMAT:xxx');
+	throw new Error('no format specified. --environment FORMAT:xxx');
 }
 
 export default {
-  input: 'src/index.js',
-  output: Object.assign(
-    {
-      name: umd ? 'ReactiveSearchVue' : '@appbaseio/reactivesearch-vue',
-    },
-    output,
-  ),
-  external: umd
-    ? Object.keys(pkg.peerDependencies || {})
-    : [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-  plugins: [
-    json(),
-    babel({
-      exclude: 'node_modules/**',
-      babelrc: false,
-      presets: [
-        [
-          'env',
-          {
-            loose: true,
-            modules: false,
-          },
-        ],
-        'stage-2',
-      ],
-      plugins: ['jsx-vue-functional', 'transform-vue-jsx', 'external-helpers'],
-    }),
-    umd
-      ? resolve({
-          jsnext: true,
-          main: true,
-          browser: true,
-          preferBuiltins: true,
-        })
-      : {},
-    umd
-      ? commonjs({
-          include: 'node_modules/**',
-        })
-      : {},
-    umd
-      ? replace({
-          'process.env.NODE_ENV': JSON.stringify(minify ? 'production' : 'development'),
-        })
-      : null,
-    minify ? terser() : null,
-  ].filter(Boolean),
+	input: 'src/index.js',
+	output: Object.assign(
+		{
+			name: umd ? 'ReactiveSearchVue' : '@appbaseio/reactivesearch-vue',
+		},
+		output,
+	),
+	external: umd
+		? Object.keys(pkg.peerDependencies || {})
+		: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+	plugins: [
+		json(),
+		babel({
+			...(!umd && { exclude: 'node_modules/**' }),
+			babelrc: false,
+			presets: [
+				[
+					'env',
+					{
+						loose: true,
+						modules: false,
+					},
+				],
+				'stage-2',
+			],
+			plugins: ['jsx-vue-functional', 'transform-vue-jsx', 'external-helpers'],
+		}),
+		umd
+			? resolve({
+				browser: true,
+				preferBuiltins: false,
+				customResolveOptions: {
+					moduleDirectory: '../reactivecore',
+				},
+			  })
+			: {},
+		umd
+			? commonjs({
+				include: 'node_modules/**',
+			  })
+			: {},
+		umd
+			? replace({
+				'process.env.NODE_ENV': JSON.stringify(minify ? 'production' : 'development'),
+			  })
+			: null,
+		minify ? terser() : null,
+	].filter(Boolean),
 };
