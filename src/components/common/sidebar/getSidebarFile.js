5085ad79f5a0b55d25b70b3188bad77dba14595a
jyash97 <jyash97@gmail.com>
2019-04-30 18:01:21 +0530

    Update console messages

--- src/components/common/sidebar/getSidebarFile.js
+++ src/components/common/sidebar/getSidebarFile.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 
 const getSidebarFile = sidebar => {
-	console.log('Here', sidebar);
-
 	try {
 		// declare as var here, so it's accessible outside of the try scope
 		var [sidebarfile] = require(`../../../data/sidebars/${sidebar}.yaml`);
 	} catch (e) {
 		// TODO: make clear error handling here
-		console.error(e);
+		console.error('Error', e);
 		throw e;
 	}
 
