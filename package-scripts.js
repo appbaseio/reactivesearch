7c73c33a93cb3067cd97e8e5adc3dfa7c89f9275
kuldeep <kuldepsaxena155@gmail.com>
2019-05-10 04:58:32 +0530

    build(web): v3.0.0-rc.7

--- package-scripts.js
+++ package-scripts.js
@@ -1,9 +1,13 @@
 const packageJSON = require('./packages/web/package.json');
+const path = require('path');
 
 module.exports = {
 	scripts: {
-		web: {
-			version: `scripts/version-update.sh "@appbaseio/reactivesearch" ${packageJSON.version}`,
+		upgrade: {
+			description: 'Updates the ReactiveSearch version in all over the mono-repo.',
+			default: `${path.resolve(
+				`${__dirname}/scripts/version-update.sh`,
+			)} "@appbaseio/reactivesearch" ${packageJSON.version}`,
 		},
 	},
 };
