2cce83f3edf653fc1add57785d1ed3acda3ff75a
Tobias Westfeld <tobias.westfeld@eshaker.de>
2019-08-29 12:00:17 +0200

    fix(web): force ReactiveList to re-render if customRender changes (#1124)
    * fix(web): force ReactiveList to re-render if customRender changes
    
    * fix(web): remove shouldComponentUpdate from Results component

--- packages/web/src/components/result/addons/Results.js
+++ packages/web/src/components/result/addons/Results.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { getClassName, isEqual } from '@appbaseio/reactivecore/lib/utils/helper';
+import { getClassName } from '@appbaseio/reactivecore/lib/utils/helper';
 import types from '@appbaseio/reactivecore/lib/utils/types';
 
 class Results extends React.Component {
-	shouldComponentUpdate(nextProps) {
-		/*
-			Use shouldComponentUpdate because PureComponent uses shallow comparison of objects.
-			If props contain complex data structures, it may produce false-negatives for
-			deeper differences.
-		*/
-
-		return !isEqual(nextProps.filteredResults, this.props.filteredResults);
-	}
-
 	render() {
 		if (this.props.hasCustomRender) {
 			return this.props.getComponent();
