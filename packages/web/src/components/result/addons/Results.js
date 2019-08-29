4f2dbc9f6d887e76fe2e6ad9539e0ac16ad0f24a
Tobias Westfeld <tobias.westfeld@eshaker.de>
2019-08-29 10:21:20 +0200

    fix(web): remove shouldComponentUpdate from Results component

--- packages/web/src/components/result/addons/Results.js
+++ packages/web/src/components/result/addons/Results.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import { getClassName, isEqual } from '@appbaseio/reactivecore/lib/utils/helper';
+import { getClassName } from '@appbaseio/reactivecore/lib/utils/helper';
 import types from '@appbaseio/reactivecore/lib/utils/types';
 
 class Results extends React.Component {
-	shouldComponentUpdate(nextProps) {
-		/*
-			Use shouldComponentUpdate because PureComponent uses shallow comparison of objects.
-			If props contain complex data structures, it may produce false-negatives for
-			deeper differences.
-		*/
-
-		return !(
-			isEqual(nextProps.filteredResults, this.props.filteredResults)
-			&& isEqual(nextProps.hasCustomRender, this.props.hasCustomRender)
-			&& isEqual(nextProps.getComponent, this.props.getComponent)
-			&& isEqual(nextProps.renderItem, this.props.renderItem)
-		);
-	}
-
 	render() {
 		if (this.props.hasCustomRender) {
 			return this.props.getComponent();
