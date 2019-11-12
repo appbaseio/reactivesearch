9e4fcb46f0adbafcf85c8a730be8638932fbb046
anuj <anujshah584@gmail.com>
2019-11-12 18:58:40 +0530

    feat: add examples RL with/without aggs

---
+++ packages/vue/examples/reactivelist-without-aggregation/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+	<div id="app">
+		<ReactiveBase
+			app="carstore-dataset-latest"
+			credentials="B86d2y2OE:4fecb2c5-5c5f-49e5-9e0b-0faba74597c6"
+		>
+			<ReactiveList
+				componentId="SearchResult"
+				dataField="brand.raw"
+				className="result-list-container"
+			>
+				<div slot="renderAllData" slot-scope="{ results }">
+					<div v-bind:key="result._id" v-for="result in results">
+						<h3>{{ result.brand }}</h3>
+						<img :src="result.image" width="500" />
+					</div>
+				</div>
+			</ReactiveList>
+		</ReactiveBase>
+	</div>
+</template>
+
+<script>
+import './styles.css';
+
+export default {
+	name: 'app',
+};
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
