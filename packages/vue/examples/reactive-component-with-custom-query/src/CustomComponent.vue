66daa3c195c04adac48df4b28259eec25e4bcae5
anuj <anujshah584@gmail.com>
2019-11-22 17:08:12 +0530

    chore(vue): add ReactiveComponent with customQuery example

---
+++ packages/vue/examples/reactive-component-with-custom-query/src/CustomComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-if="aggregations">
+        <div
+            :style="{cursor: 'pointer' }"
+            v-for="item in aggregations['brand.keyword'].buckets"
+            v-bind:key="item.id"
+            @click="() => setValue(item.key)"
+        >
+            <b v-if="selected === item.key" :style="{color: 'blue' }">
+                {{ item.key }}
+            </b>
+            <span v-else> {{ item.key }} </span>
+        </div>
+    </div>
+</template>
+    <script>
+        export default {
+            name: "CustomComponent",
+  props: {
+            subProps: Object,
+        subEvents: Object,
+        aggregations: Object,
+        setQuery: Function
+      },
+  data: function() {
+    return {
+            selected: ""
+      };
+    },
+  methods: {
+            setValue(value) {
+        this.$props.setQuery({
+            query: {
+            term: {
+            "brand.keyword": value
+      }
+    },
+    value
+  });
+  return (this.selected = value);
+}
+}
+};
+</script>
