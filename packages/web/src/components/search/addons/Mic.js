40885fe62618a34b660bcd6347c1567e4aff41c5
anuj <anujshah584@gmail.com>
2020-05-05 12:20:55 +0530

    fix(web): mic state for not supported browsers
    ## What does this PR do?
    - set mic state to denied if browser does not support SpeechRecognition
    
    ## Test
    https://www.loom.com/share/a4d29aac186a4693954eb36771bdbed6

--- packages/web/src/components/search/addons/Mic.js
+++ packages/web/src/components/search/addons/Mic.js
@@ -14,11 +14,9 @@ const STATUS = {
 };
 
 class Mic extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			status: STATUS.inactive,
-		};
+	constructor(props) {
+		super(props);
+		let status = STATUS.inactive;
 		this.results = [];
 
 		if (typeof window !== 'undefined') {
@@ -26,11 +24,15 @@ class Mic extends React.Component {
 				= window.webkitSpeechRecognition || window.SpeechRecognition || null;
 
 			if (!window.SpeechRecognition) {
+				status = STATUS.denied;
 				console.error(
 					'SpeechRecognition is not supported in this browser. Please check the browser compatibility at https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition#Browser_compatibility.',
 				);
 			}
 		}
+		this.state = {
+			status,
+		};
 	}
 
 	stopMic = () => {
