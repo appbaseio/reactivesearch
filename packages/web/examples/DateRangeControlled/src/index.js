6dbfeb286a1475d88266201f0ad626560755f126
Anuj Shah <anujshah584@gmail.com>
2020-05-07 18:51:45 +0530

    feat(web): controlled component behavior for DateRange (#1426)
    * feat(web): controlled component behavior for DateRange
    
    * Revert yarn.lock
    
    * fix(web): daterange for controlled component

---
+++ packages/web/examples/DateRangeControlled/src/index.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import moment from 'moment';
+
+import {
+	ReactiveBase,
+	DateRange,
+	ResultCard,
+	SelectedFilters,
+	ReactiveList,
+} from '@appbaseio/reactivesearch';
+
+import './index.css';
+
+class Main extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { date: { start: new Date(), end: new Date() } };
+	}
+
+	dateQuery(value) {
+		let query = null;
+		if (value) {
+			query = [
+				{
+					range: {
+						date_from: {
+							gte: moment(value.start).format('YYYYMMDD'),
+						},
+					},
+				},
+				{
+					range: {
+						date_to: {
+							lte: moment(value.end).format('YYYYMMDD'),
+						},
+					},
+				},
+			];
+		}
+		return query ? { query: { bool: { must: query } } } : null;
+	}
+
+	render() {
+		return (
+			<ReactiveBase
+				app="airbeds-test-app"
+				credentials="X8RsOu0Lp:9b4fe1a4-58c6-4089-a042-505d86d9da30"
+				type="listing"
+			>
+				<div className="row">
+					<div className="col">
+						<DateRange
+							value={this.state.date}
+							onChange={(value) => {
+								// setting end date equal to start date
+								this.setState({
+									date: {
+										start: value.start,
+										end: value.start,
+									},
+								});
+							}}
+							componentId="DateSensor"
+							dataField="date_from"
+							customQuery={this.dateQuery}
+							initialMonth={new Date('2017-05-05')}
+						/>
+					</div>
+
+					<div className="col">
+						<SelectedFilters />
+						<ReactiveList
+							componentId="SearchResult"
+							dataField="name"
+							from={0}
+							size={40}
+							showPagination
+							react={{
+								and: ['DateSensor'],
+							}}
+							render={({ data }) => (
+								<ReactiveList.ResultCardsWrapper>
+									{data.map(item => (
+										<ResultCard href={item.listing_url} key={item.id}>
+											<ResultCard.Image src={item.image} />
+											<ResultCard.Title>
+												<div
+													className="book-title"
+													dangerouslySetInnerHTML={{
+														__html: item.name,
+													}}
+												/>
+											</ResultCard.Title>
+
+											<ResultCard.Description>
+												<div>
+													<div>${item.price}</div>
+													<span
+														style={{
+															backgroundImage: `url(${item.host_image})`,
+														}}
+													/>
+													<p>
+														{item.room_type} · {item.accommodates}{' '}
+														guests
+													</p>
+												</div>
+											</ResultCard.Description>
+										</ResultCard>
+									))}
+								</ReactiveList.ResultCardsWrapper>
+							)}
+						/>
+					</div>
+				</div>
+			</ReactiveBase>
+		);
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
