e9aa7645cd461abb14561b03cabc761d65b9e701
anuj <anujshah584@gmail.com>
2020-05-26 14:10:21 +0530

    fix: emotion for ssr

--- packages/web/examples/ssr/pages/_document.js
+++ packages/web/examples/ssr/pages/_document.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-import { extractCritical } from 'emotion-server';
+import { renderToString } from 'react-dom/server';
 
 export default class MyDocument extends Document {
 	static getInitialProps({ renderPage }) {
 		// for emotion-js
 		const page = renderPage();
-		const styles = extractCritical(page.html);
+		const styles = renderToString(page.html);
 		return { ...page, ...styles };
 	}
 
