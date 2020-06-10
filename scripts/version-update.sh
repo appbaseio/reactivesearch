d9ab875a3b10a78f157e51021ca931f1aead1397
jyash97 <jyash97@gmail.com>
2020-06-10 16:46:02 +0530

    chore: remove logs

--- scripts/version-update.sh
+++ scripts/version-update.sh
@@ -33,7 +33,6 @@ var walk = function(dir,done) {
 walk("$var", function(err, results) {
     if (err) throw err;
     var filesToUpdate = results.filter(item => item.includes("package.json")).filter(item => !item.includes("node_modules") && !item.includes("/."));
-    console.log(filesToUpdate)
     filesToUpdate.forEach(file => {
         var jsonFile = require(file);
         if(true && jsonFile.dependencies && jsonFile.dependencies["$1"]){
