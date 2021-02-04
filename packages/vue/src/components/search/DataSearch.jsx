10be4848a94613478b4187cae1c805cef1723d69
Anik Ghosh <tech.anikghosh@gmail.com>
2021-02-04 16:02:00 +0530

    fix(vue): phrase_perfix query added in DataSearch

--- packages/vue/src/components/search/DataSearch.jsx
+++ packages/vue/src/components/search/DataSearch.jsx
@@ -907,38 +907,64 @@ DataSearch.defaultQuery = (value, props) => {
 	return finalQuery;
 };
 DataSearch.shouldQuery = (value, dataFields, props) => {
-	const fields = dataFields.map(
-		(field, index) =>
-			`${field}${
+	const finalQuery = [];
+		const phrasePrefixFields = [];
+		const fields = dataFields.map((field, index) => {
+			const queryField = `${field}${
 				Array.isArray(props.fieldWeights) && props.fieldWeights[index]
 					? `^${props.fieldWeights[index]}`
 					: ''
-			}`,
-	);
+			}`;
+			if (
+				!(
+					field.endsWith('.keyword')
+					|| field.endsWith('.autosuggest')
+					|| field.endsWith('.search')
+				)
+			) {
+				phrasePrefixFields.push(queryField);
+			}
+			return queryField;
+		});
+		if (props.searchOperators || props.queryString) {
+			return {
+				query: value,
+				fields,
+				default_operator: props.queryFormat,
+			};
+		}
 
-	if (props.queryFormat === 'and') {
-		return [
-			{
+		if (props.queryFormat === 'and') {
+			finalQuery.push({
 				multi_match: {
 					query: value,
 					fields,
 					type: 'cross_fields',
 					operator: 'and',
 				},
-			},
-			{
+			});
+			finalQuery.push({
 				multi_match: {
 					query: value,
 					fields,
 					type: 'phrase',
 					operator: 'and',
 				},
-			},
-		];
-	}
+			});
+			if (phrasePrefixFields.length > 0) {
+				finalQuery.push({
+					multi_match: {
+						query: value,
+						fields: phrasePrefixFields,
+						type: 'phrase_prefix',
+						operator: 'and',
+					},
+				});
+			}
+			return finalQuery;
+		}
 
-	return [
-		{
+		finalQuery.push({
 			multi_match: {
 				query: value,
 				fields,
@@ -946,16 +972,29 @@ DataSearch.shouldQuery = (value, dataFields, props) => {
 				operator: 'or',
 				fuzziness: props.fuzziness ? props.fuzziness : 0,
 			},
-		},
-		{
+		});
+
+		finalQuery.push({
 			multi_match: {
 				query: value,
 				fields,
 				type: 'phrase',
 				operator: 'or',
 			},
-		},
-	];
+		});
+
+		if (phrasePrefixFields.length > 0) {
+			finalQuery.push({
+				multi_match: {
+					query: value,
+					fields: phrasePrefixFields,
+					type: 'phrase_prefix',
+					operator: 'or',
+				},
+			});
+		}
+
+		return finalQuery;
 };
 DataSearch.highlightQuery = props => {
 	if (props.customHighlight) {
