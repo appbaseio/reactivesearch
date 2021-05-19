510ee0e587078f687f32f18d61d71bd01af6236e
Anik Ghosh <tech.anikghosh@gmail.com>
2021-05-19 13:16:56 +0530

    feat(vue): range-input component added in vue-searchbox

---
+++ packages/vue/src/components/range/RangeInput.jsx
@@ -0,0 +1,236 @@
+import VueTypes from 'vue-types';
+import { helper } from '@appbaseio/reactivecore';
+import { componentTypes } from '@appbaseio/reactivecore/lib/utils/constants';
+import Container from '../../styles/Container';
+import { connect, updateCustomQuery, isQueryIdentical } from '../../utils/index';
+import ComponentWrapper from '../basic/ComponentWrapper.jsx';
+import types from '../../utils/vueTypes';
+import { RangeConnected as RangeSlider } from './RangeSlider.jsx';
+import Input from '../../styles/Input';
+import Content from '../../styles/Content';
+import Flex from '../../styles/Flex';
+
+const { checkValueChange, getClassName, getOptionsFromQuery, isEqual } = helper;
+
+const RangeInput = {
+	name: 'RangeInput',
+	components: {
+		RangeSlider,
+	},
+	inject: {
+		theme: {
+			from: 'theme_reactivesearch',
+		},
+	},
+	data() {
+		const state = {
+			currentValue: {
+				start: this.$props.range ? this.$props.range.start : 0,
+				end: this.$props.range ? this.$props.range.end : 10
+			},
+			isStartValid: true,
+			isEndValid: true
+		};
+		return state;
+	},
+
+	props: {
+		className: {
+			types: types.string,
+			default: ''
+		},
+		defaultValue: types.range,
+		validateRange: types.func,
+		value: types.range,
+		dataField: types.stringRequired,
+		innerClass: types.style,
+		range: {
+			types: types.range,
+			default() {
+				return {
+					start: 0,
+					end: 10
+				}
+			}
+		},
+		rangeLabels: types.rangeLabels,
+		stepValue: types.number,
+		componentStyle: types.style,
+		componentId: types.stringRequired,
+		includeNullValues: types.bool,
+	},
+
+	methods: {
+		shouldUpdate(value) {
+			const { validateRange } = this.$props;
+			if (validateRange) {
+				return validateRange([value.start, value.end]);
+			}
+			return true;
+		},
+		isControlled() {
+			if (this.$props.value && this.$listeners) {
+				return true;
+			}
+			return false;
+		},
+		handleChange(value, event) {
+			if (this.shouldUpdate(value)) {
+				this.$data.currentValue = value;
+				switch (event) {
+					case 'change':
+						this.$emit('change', this.$data.currentValue);
+						break;
+					case 'value-change':
+						this.$emit('valueChange', this.$data.currentValue);
+						this.$emit('value-change', this.$data.currentValue);
+						break;
+					case 'none':
+						break;
+					default:
+						this.$emit('change', this.$data.currentValue);
+						this.$emit('valueChange', this.$data.currentValue);
+						this.$emit('value-change', this.$data.currentValue);
+						break;
+				}
+			}
+		},
+		handleOnChange(value) {
+			this.handleChange(value, 'change');
+		},
+		handleValueChange(value) {
+			this.handleChange(value, 'value-change');
+		},
+		handleInputChange(e) {
+			const { name, value } = e.target;
+			if (Number.isNaN(value)) {
+				if (name === 'start') {
+					this.$data.isStartValid = false;
+				} else {
+					this.$data.isEndValid = false;
+				}
+			} else {
+				if (name === 'start' && !this.$data.isStartValid) {
+					this.$data.isStartValid = true;
+				} else if (name === 'end' && !this.$data.isEndValid) {
+					this.$data.isEndValid = true;
+				}
+			}
+
+			if (this.$data.isStartValid && this.$data.isEndValid) {
+				if (name === 'start') {
+					this.handleChange({
+						start: Number(value),
+						end: this.$data.currentValue.end
+					});
+				} else {
+					this.handleChange({
+						start: this.$data.currentValue.start,
+						end: Number(value)
+					});
+				}
+			}
+		}
+	},
+	watch: {
+		value(newVal, oldVal) {
+			if (!isEqual(newVal, oldVal)) {
+				if (this.isControlled()) {
+					this.handleChange(newVal, 'value-change');
+				}
+			}
+		},
+	},
+	created() {
+		if (this.$props.defaultValue && this.$props.defaultValue.start && this.$props.defaultValue.end) {
+			this.handleChange(this.$props.defaultValue, 'none')
+		}
+	},
+	render() {
+		const {
+			className,
+			dataField,
+			range,
+			rangeLabels,
+			componentId,
+			innerClass,
+			stepValue,
+			componentStyle,
+			themePreset,
+			includeNullValues
+		} = this.$props;
+		return (
+			<Container style={componentStyle} class={className}>
+				<RangeSlider
+					componentId={componentId}
+					value={{
+						start: this.$data.currentValue.start,
+						end: this.$data.currentValue.end
+					}}
+					range={range}
+					dataField={dataField}
+					rangeLabels={rangeLabels}
+					includeNullValues={includeNullValues}
+					on-change={this.handleOnChange}
+					on-value-change={this.handleValueChange}
+				/>
+				<Flex className={getClassName(innerClass, 'input-container') || null}>
+					<Flex direction="column" flex={2}>
+						<Input
+							key={this.$data.currentValue.start}
+							name="start"
+							type="number"
+							onChange={this.handleInputChange}
+							value={this.$data.currentValue.start}
+							step={stepValue}
+							themePreset={themePreset}
+							aria-label={`${componentId}-start-input`}
+							min={this.$props.range ? this.$props.range.start : 0}
+							className={getClassName(innerClass, 'input') || null}
+							alert={!this.$data.isStartValid}
+						/>
+						{!this.$data.isStartValid && (
+							<Content alert>Input range is invalid</Content>
+						)}
+					</Flex>
+					<Flex justifyContent="center" alignItems="center" flex={1}>
+						-
+					</Flex>
+					<Flex direction="column" flex={2}>
+						<Input
+							key={this.$data.currentValue.end}
+							name="end"
+							type="number"
+							onChange={this.handleInputChange}
+							value={this.$data.currentValue.end}
+							step={stepValue}
+							themePreset={themePreset}
+							aria-label={`${componentId}-end-input`}
+							max={this.$props.range ? this.$props.range.end : 10}
+							className={getClassName(innerClass, 'input') || null}
+							alert={!this.$data.isEndValid}
+						/>
+						{!this.$data.isEndValid && <Content alert>Input range is invalid</Content>}
+					</Flex>
+				</Flex>
+			</Container>
+		);
+	},
+};
+
+const mapStateToProps = (state, props) => ({
+	themePreset: state.config.themePreset,
+});
+
+const RangeConnected = ComponentWrapper(connect(mapStateToProps, {})(RangeInput), {
+	componentType: componentTypes.rangeInput,
+});
+
+RangeInput.install = function(Vue) {
+	Vue.component(RangeInput.name, RangeConnected);
+};
+
+// Add componentType for SSR
+RangeInput.componentType = componentTypes.rangeInput;
+
+export default RangeInput;
