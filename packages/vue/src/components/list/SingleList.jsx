ac8766d02a4b6c4c3f3665e0cd343c464d3074f0
Anik Ghosh <tech.anikghosh@gmail.com>
2021-06-07 13:58:45 +0530

    feat(vue): support for enable-strict-selection prop added (#1708)
    * feat(vue): support for enable-strict-selection prop added
    
    * fix(vue): enable-strict-selection prop removed from some components
    
    * fix(vue): enable strict selction prop updated
    
    * fix(vue): enable-strict-selection logic updated in single-list component
    
    * fix(vue): formatting updates

--- packages/vue/src/components/list/SingleList.jsx
+++ packages/vue/src/components/list/SingleList.jsx
@@ -53,6 +53,7 @@ const SingleList = {
 		showMissing: VueTypes.bool.def(false),
 		missingLabel: VueTypes.string.def('N/A'),
 		nestedField: types.string,
+		enableStrictSelection: VueTypes.bool.def(false),
 	},
 	data() {
 		const props = this.$props;
@@ -124,7 +125,13 @@ const SingleList = {
 		},
 	},
 	render() {
-		const { selectAllLabel, renderItem, renderError, renderNoResults } = this.$props;
+		const {
+			selectAllLabel,
+			renderItem,
+			renderError,
+			renderNoResults,
+			enableStrictSelection,
+		} = this.$props;
 		const renderItemCalc = this.$scopedSlots.renderItem || renderItem;
 		const renderErrorCalc = this.$scopedSlots.renderError || renderError;
 
@@ -141,17 +148,17 @@ const SingleList = {
 			itemsToRender = this.$props.transformData(itemsToRender);
 		}
 
-		var filteredItemsToRender = itemsToRender.filter(item => {
+		const filteredItemsToRender = itemsToRender.filter(item => {
 			if (String(item.key).length) {
 				if (this.$props.showSearch && this.$data.searchTerm) {
 					return String(item.key)
 						.toLowerCase()
 						.includes(this.$data.searchTerm.toLowerCase());
-					}
-					return true;
 				}
-				return false;
-			});
+				return true;
+			}
+			return false;
+		});
 
 		return (
 			<Container class={this.$props.className}>
@@ -194,10 +201,11 @@ const SingleList = {
 								</label>
 							</li>
 						) : null}
-						{(!this.hasCustomRenderer && filteredItemsToRender.length === 0
-						&& !this.isLoading ) ? this.renderNoResult() :
-						filteredItemsToRender
-							.map(item => (
+						{!this.hasCustomRenderer
+						&& filteredItemsToRender.length === 0
+						&& !this.isLoading
+							? this.renderNoResult()
+							: filteredItemsToRender.map(item => (
 								<li
 									key={item.key}
 									class={`${
@@ -210,7 +218,12 @@ const SingleList = {
 										name={this.$props.componentId}
 										value={item.key}
 										readOnly
-										onClick={this.handleClick}
+										onClick={
+											this.$data.currentValue === String(item.key)
+												&& enableStrictSelection
+												? () => false
+												: this.handleClick
+										}
 										type="radio"
 										show={this.$props.showRadio}
 										{...{
@@ -221,7 +234,8 @@ const SingleList = {
 									/>
 									<label
 										class={
-											getClassName(this.$props.innerClass, 'label') || null
+											getClassName(this.$props.innerClass, 'label')
+												|| null
 										}
 										for={`${this.$props.componentId}-${item.key}`}
 									>
@@ -229,7 +243,8 @@ const SingleList = {
 											renderItemCalc({
 												label: item.key,
 												count: item.doc_count,
-												isChecked: this.currentValue === String(item.key),
+												isChecked:
+														this.currentValue === String(item.key),
 											})
 										) : (
 											<span>
@@ -243,7 +258,7 @@ const SingleList = {
 															) || null
 														}
 													>
-														&nbsp;(
+															&nbsp;(
 														{item.doc_count})
 													</span>
 												)}
@@ -251,7 +266,7 @@ const SingleList = {
 										)}
 									</label>
 								</li>
-							))}
+							  ))}
 					</UL>
 				)}
 			</Container>
@@ -265,7 +280,6 @@ const SingleList = {
 			if (nextValue === this.$data.currentValue) {
 				value = '';
 			}
-
 			const performUpdate = () => {
 				this.currentValue = value;
 				this.updateQueryHandler(value, props);
