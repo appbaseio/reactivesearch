cf3920eb54ecbc9d3d13bc608acd101ed4429a72
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-08-12 15:50:15 +0530

    chore: remove stream prop and usage (#1744)

--- demos/polyline/src/index.js
+++ demos/polyline/src/index.js
@@ -51,7 +51,7 @@ class Main extends React.Component {
 					size={50}
 					defaultZoom={3}
 					center={{ lat: 0, lng: -180 }}
-					renderAllData={(hits, streamHits, loadMore, renderMap) => {
+					renderAllData={(hits, loadMore, renderMap) => {
 						if (this.mapRef) {
 							const { map } = this.mapRef;
 							const flightPath = new window.google.maps.Polyline({
