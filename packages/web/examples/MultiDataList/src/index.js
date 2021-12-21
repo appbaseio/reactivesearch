4a509d7a4ec7ccf766e479a433aae0a5f037aa56
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-12-21 19:29:35 +0530

    fix: disabled query rules as it broke the example

--- packages/web/examples/MultiDataList/src/index.js
+++ packages/web/examples/MultiDataList/src/index.js
@@ -16,6 +16,9 @@ const Main = () => (
 		app="meetup_app"
 		url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
 		enableAppbase
+		appbaseConfig={{
+			enableQueryRules: false,
+		}}
 	>
 		<div className="row">
 			<div className="col">
