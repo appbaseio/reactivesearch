1de4a6c5e3b949213d3877e43535f68d5b84208b
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-01-07 00:28:01 +0530

    feat(web): setup jest config file for serializer and local dev setup

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,5 +1,7 @@
 // jest.config.js
 module.exports = {
+	verbose: true,
+	testURL: 'http://localhost/',
 	// ... other config
 	snapshotSerializers: [
 		'@emotion/jest/serializer' /* if needed other snapshotSerializers should go here */,
