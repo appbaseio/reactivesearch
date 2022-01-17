8623935409b96acdf89a1a0f482e76bfec0ecb5b
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-01-17 22:01:52 +0530

    refactor: the getComponent and hadcustomrenderer method to carefully check the render slot or prop

--- packages/vue/src/utils/index.js
+++ packages/vue/src/utils/index.js
@@ -55,7 +55,9 @@ export function parseValueArray(objectValues, currentValue) {
  * @param _ref
  */
 export const getComponent = (data = {}, _ref = {}) => {
-	const { render } = _ref.$scopedSlots || _ref.$props;
+	const { render: renderScope } = _ref.$scopedSlots || {};
+	const { render: renderProp } = _ref.$props || {};
+	const render = renderScope || renderProp;
 	if (render) return render(data);
 	return null;
 };
@@ -64,8 +66,9 @@ export const getComponent = (data = {}, _ref = {}) => {
  * @returns {Boolean}
  */
 export const hasCustomRenderer = (_ref = {}) => {
-	const { render } = _ref.$scopedSlots || _ref.$props;
-	return Boolean(render);
+	const { render: renderScope } = _ref.$scopedSlots || {};
+	const { render: renderProp } = _ref.$props || {};
+	return Boolean(renderScope || renderProp);
 };
 
 export const getValidPropsKeys = (props = {}) =>
