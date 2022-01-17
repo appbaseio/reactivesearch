8fd9e221fe73482fec5627884e6c2ba539922f70
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-01-17 18:59:52 +0530

    feat(vue): configured component wrapper for snapshot testing

--- packages/vue/src/components/basic/ComponentWrapper.jsx
+++ packages/vue/src/components/basic/ComponentWrapper.jsx
@@ -12,6 +12,7 @@ const {
 	setQueryListener,
 	setComponentProps,
 	updateComponentProps,
+	mockDataForTesting,
 } = Actions;
 
 const { pushToAndClause, checkPropChange, checkSomePropChange } = helper;
@@ -49,6 +50,13 @@ const ComponentWrapper = (
 		this.componentProps = parsedProps;
 		this.componentId = this.componentProps.componentId;
 		this.react = this.componentProps.react;
+
+		if (this.componentProps.mockData) {
+			this.mockDataForTesting(
+				this.componentProps.componentId,
+				this.componentProps.mockData,
+			);
+		}
 	},
 	beforeMount() {
 		let components = [];
@@ -94,7 +102,7 @@ const ComponentWrapper = (
 		}
 	},
 	mounted() {
-		if (this.internalComponent) {
+		if (this.internalComponent && this.componentProps.mode !== 'test') {
 			// Watch component after rendering the component to avoid the un-necessary calls
 			this.setReact(this.componentProps);
 		}
@@ -164,6 +172,7 @@ const mapDispatchToProps = {
 	watchComponent,
 	setComponentProps,
 	updateComponentProps,
+	mockDataForTesting,
 };
 export default (component, options = {}) =>
 	connect(mapStateToProps, mapDispatchToProps)(ComponentWrapper(component, options));
