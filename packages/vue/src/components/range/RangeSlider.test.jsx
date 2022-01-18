dc8eb1a23cad9e713ebbabea7fdbf33409a98a94
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-01-18 11:50:17 +0530

    feat(vue): added sanpshot tests for RangeSlider component

---
+++ packages/vue/src/components/range/RangeSlider.test.jsx
@@ -0,0 +1,143 @@
+import { mount } from '@vue/test-utils';
+import Vue from 'vue';
+import { RangeConnected as RangeSlider } from './RangeSlider.jsx';
+import ReactiveBase from '../ReactiveBase/index.jsx';
+
+Vue.component('vue-slider-component', require('vue-slider-component'));
+
+describe('RangeSlider', () => {
+	it('should render range with slider', () => {
+		const wrapper = mount({
+			name: 'TestComponent',
+			render() {
+				return (
+					<ReactiveBase app="test" url="https://foo:bar@localhost:800">
+						<RangeSlider
+							mode="test"
+							componentId="authors"
+							dataField="ratings_count"
+							range={{
+								start: 30000,
+								end: 50000,
+							}}
+						/>
+					</ReactiveBase>
+				);
+			},
+		});
+
+		expect(wrapper.element).toMatchSnapshot();
+	});
+
+	it('should render title', () => {
+		const wrapper = mount({
+			name: 'TestComponent',
+			render() {
+				return (
+					<ReactiveBase app="test" url="https://foo:bar@localhost:800">
+						<RangeSlider
+							mode="test"
+							componentId="authors"
+							dataField="ratings_count"
+							range={{
+								start: 30000,
+								end: 50000,
+							}}
+							title="Test Title"
+						/>
+					</ReactiveBase>
+				);
+			},
+		});
+
+		expect(wrapper.element).toMatchSnapshot();
+	});
+
+	it('should render range labels', () => {
+		const wrapper = mount({
+			name: 'TestComponent',
+			render() {
+				return (
+					<ReactiveBase app="test" url="https://foo:bar@localhost:800">
+						<RangeSlider
+							mode="test"
+							componentId="authors"
+							dataField="ratings_count"
+							range={{
+								start: 30000,
+								end: 50000,
+							}}
+							rangeLabels={{
+								start: '30K',
+								end: '50K',
+							}}
+						/>
+					</ReactiveBase>
+				);
+			},
+		});
+
+		expect(wrapper.element).toMatchSnapshot();
+	});
+
+	it('should select default value', () => {
+		const wrapper = mount({
+			name: 'TestComponent',
+			render() {
+				return (
+					<ReactiveBase app="test" url="https://foo:bar@localhost:800">
+						<RangeSlider
+							mode="test"
+							componentId="authors"
+							dataField="ratings_count"
+							range={{
+								start: 3000,
+								end: 5500,
+							}}
+							rangeLabels={{
+								start: '3K',
+								end: '5.5K',
+							}}
+							defaultValue={{
+								start: 3200,
+								end: 4500,
+							}}
+						/>
+					</ReactiveBase>
+				);
+			},
+		});
+
+		expect(wrapper.element).toMatchSnapshot();
+	});
+
+	it('should not display tooltip when sliderOptions had tooltip as none', () => {
+		const wrapper = mount({
+			name: 'TestComponent',
+			render() {
+				return (
+					<ReactiveBase app="test" url="https://foo:bar@localhost:800">
+						<RangeSlider
+							mode="test"
+							componentId="authors"
+							dataField="ratings_count"
+							range={{
+								start: 3000,
+								end: 5500,
+							}}
+							rangeLabels={{
+								start: '3K',
+								end: '5.5K',
+							}}
+							sliderOptions={{
+								tooltip: false,
+							}}
+						/>
+					</ReactiveBase>
+				);
+			},
+		});
+
+		expect(wrapper.element).toMatchSnapshot();
+	});
+});
