00a8b7322adf815cbf762739162873ed86c625ce
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-04-26 12:56:25 +0530

    formatting change

--- packages/web/src/styles/Button.js
+++ packages/web/src/styles/Button.js
@@ -94,8 +94,8 @@ const large = css`
 
 const disabled = ({ theme }) => css`
 	background-color: ${theme.colors.backgroundColor
-		? lighten(0.1, theme.colors.backgroundColor)
-		: '#fafafa'};
+	? lighten(0.1, theme.colors.backgroundColor)
+	: '#fafafa'};
 	color: #ccc;
 	cursor: not-allowed;
 
@@ -126,7 +126,7 @@ const Button = styled('a')`
 	&:hover,
 	&:focus {
 		background-color: ${({ theme }) =>
-		(theme.colors.backgroundColor ? darken(0.1, theme.colors.backgroundColor) : '#ccc')};
+	(theme.colors.backgroundColor ? darken(0.1, theme.colors.backgroundColor) : '#ccc')};
 	}
 
 	&:focus {
