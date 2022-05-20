d70dca7b5bfbae2ee1f440d80ac501f468add8bc
Shubham Shah <52590791+SavvyShah@users.noreply.github.com>
2022-05-20 20:12:03 +0530

    Docs multi index (#1961)
    * Add a multi index search example
    
    * Make an example on multifacet search
    
    * fix(web): Results from both index on first page

---
+++ packages/web/examples/MultiIndexFacet/src/index.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import { ReactiveBase, MultiList, ReactiveList, SelectedFilters } from '@appbaseio/reactivesearch';
+
+import './index.css';
+
+class Main extends Component {
+	render() {
+		return (
+			<ReactiveBase
+				app="good-books-ds,good-books-authors"
+				url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
+				enableAppbase
+			>
+				<div className="row">
+					<div className="col">
+						<MultiList
+							componentId="BookSensor"
+							dataField="author_name.keyword"
+							index="good-books-authors"
+							// customQuery: If none is selected then match all documents else match selected
+							customQuery={value => (value.length ? ({
+								query: {
+									terms: {
+										'authors.keyword': value,
+									},
+								},
+							}) : ({
+								query: {
+									match_all: {},
+								},
+							}))}
+							aggregationSize={100}
+						/>
+					</div>
+
+					<div className="col">
+						<SelectedFilters />
+						<ReactiveList
+							componentId="SearchResult"
+							dataField="original_title"
+							className="result-list-container"
+							index="good-books-ds"
+							from={0}
+							size={5}
+							renderItem={this.booksReactiveList}
+							react={{
+								and: ['BookSensor'],
+							}}
+						/>
+					</div>
+				</div>
+			</ReactiveBase>
+		);
+	}
+
+	booksReactiveList(data) {
+		return (
+			<div className="flex book-content" key={data._id}>
+				<img src={data.image} alt="Book Cover" className="book-image" />
+				<div className="flex column justify-center" style={{ marginLeft: 20 }}>
+					<div className="book-header">{data.original_title}</div>
+					<div className="flex column justify-space-between">
+						<div>
+							<div>
+								by <span className="authors-list">{data.authors}</span>
+							</div>
+							<div className="ratings-list flex align-center">
+								<span className="stars">
+									{Array(data.average_rating_rounded)
+										.fill('x')
+										.map((item, index) => (
+											/* eslint-disable-next-line */
+											<i className="fas fa-star" key={index} />
+										))
+									}
+								</span>
+								<span className="avg-rating">({data.average_rating} avg)</span>
+							</div>
+						</div>
+						<span className="pub-year">Pub {data.original_publication_year}</span>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
---
+++ packages/web/examples/MultiIndexSearch/src/index.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+	ReactiveBase,
+	SearchBox,
+	ReactiveList,
+	ResultCard,
+	SelectedFilters,
+} from '@appbaseio/reactivesearch';
+
+import './index.css';
+
+
+const badge = {
+	'good-books': 'badgeAqua',
+	'good-books-ds': 'badgeCrimson',
+};
+
+const Main = () => (
+	<ReactiveBase
+		app="good-books,good-books-ds"
+		url="https://b59ca4ceab0d:00a2085f-8794-4a7e-96af-041f45332f0e@appbase-demo-ansible-abxiydt-arc.searchbase.io"
+		enableAppbase
+		appbaseConfig={{
+			recordAnalytics: true,
+			userId: 'jon',
+		}}
+	>
+		<div className="row">
+			<div className="col">
+				<SearchBox
+					title="SearchBox"
+					defaultValue="Ceremony"
+					dataField="original_title"
+					componentId="BookSensor"
+					size={10}
+					index="good-books,good-books-ds"
+				/>
+			</div>
+
+			<div className="col">
+				<SelectedFilters />
+				<ReactiveList
+					componentId="SearchResult"
+					dataField="original_title"
+					size={10}
+					className="result-list-container"
+					pagination
+					react={{
+						and: 'BookSensor',
+					}}
+					render={({ data }) => (
+						<ReactiveList.ResultCardsWrapper>
+							{data.map(item => (
+								<ResultCard id={item._id} key={item._id}>
+									<ResultCard.Image src={item.image} />
+									<ResultCard.Title>
+										<div
+											className="book-title"
+											dangerouslySetInnerHTML={{
+												__html: item.original_title,
+											}}
+										/>
+									</ResultCard.Title>
+
+									<ResultCard.Description>
+										<div className="flex column justify-space-between">
+											<div>
+												<div>
+													by{' '}
+													<span className="authors-list">
+														{item.authors}
+													</span>
+												</div>
+												<div className="ratings-list flex align-center">
+													<span className="stars">
+														{
+															/* eslint-disable */
+															Array(item.average_rating_rounded)
+																.fill('x')
+																.map((_, index) => (
+																	<i
+																		className="fas fa-star"
+																		key={index}
+																	/>
+																))
+															/* eslint-enable */
+														}
+													</span>
+													<span className="avg-rating">
+														({item.average_rating} avg)
+													</span>
+												</div>
+											</div>
+											<div>
+												<span className="pub-year">
+													Pub {item.original_publication_year}
+												</span>
+												<span className={badge[item._index]}>
+													{item._index}
+												</span>
+											</div>
+										</div>
+									</ResultCard.Description>
+								</ResultCard>
+							))}
+						</ReactiveList.ResultCardsWrapper>
+					)}
+				/>
+			</div>
+		</div>
+	</ReactiveBase>
+);
+
+export default Main;
+
+ReactDOM.render(<Main />, document.getElementById('root'));
