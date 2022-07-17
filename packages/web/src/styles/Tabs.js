62ec4da42ffeda9fa456f2310f3d2528082c187d
Shubham Shah <shubhamshahrising@gmail.com>
2022-07-17 23:42:17 +0530

    feat: style component

---
+++ packages/web/src/styles/Tabs.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+// eslint-disable-next-line import/prefer-default-export
+export const TabLink = styled('a')`
+	color: ${({ theme, selected }) =>
+	(selected ? theme.colors.primaryColor : theme.colors.textColor)};
+	font-weight: ${({ selected }) => (selected ? 'bold' : 'normal')};
+	padding: 1rem;
+	text-decoration: none;
+	border-color: ${({ theme }) => theme.colors.primaryColor};
+	border-width: 0px;
+	border-style: solid;
+	border-bottom-width: ${({ selected }) => (selected ? '2px' : '0px')};
+`;
