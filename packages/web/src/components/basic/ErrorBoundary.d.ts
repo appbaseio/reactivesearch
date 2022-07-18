6ee3f326bfddfb88184fb5a399843db16c5f3245
Shubham Shah <shubhamshahrising@gmail.com>
2022-07-18 09:25:14 +0530

    feat: make properties optional

--- packages/web/src/components/basic/ErrorBoundary.d.ts
+++ packages/web/src/components/basic/ErrorBoundary.d.ts
@@ -3,9 +3,9 @@ import * as types from '../../types';
 
 export interface ErrorBoundaryProps {
 	children?: types.children;
-	componentIds: types.stringArray;
-	renderError: (error: object, componentId: string)=>void;
-	onError: (error: object, componentId: string) => void;
+	componentIds?: types.stringArray;
+	renderError?: (error: object, componentId: string)=>void;
+	onError?: (error: object, componentId: string) => void;
 }
 
 declare const ErrorBoundary: React.ComponentClass<ErrorBoundaryProps>;
