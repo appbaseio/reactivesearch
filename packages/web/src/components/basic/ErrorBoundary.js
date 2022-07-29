00d87f44a73ce7be4d50388164bd875e84c578c1
Shubham Shah <52590791+SavvyShah@users.noreply.github.com>
2022-07-29 14:01:31 +0530

    Feat/error boundary (#2021)
    * feat: component with store error
    
    * feat: error boundary fixed
    
    * test: add snapshot test for ErrorBoundary
    
    * feat: make ErrorBoundary example
    
    * fix: package name clash
    
    * fix: update submodules
    
    * tests: update snapshot due to change in default error message
    
    * feat: move error boundary close to reactivebase
    
    * feat: make properties optional
    
    * feat: change error message
    
    * feat: change default error message
    
    * fix: update snapshots for errorboundary
    
    * chore: update submodules
    
    * chore: update submodules
    
    * chore: update submodule from upstream
    
    * chore: update submodule from upstream
    
    * fix: update snapshot tests

---
+++ packages/web/src/components/basic/ErrorBoundary.js
@@ -0,0 +1,69 @@
+import types from '@appbaseio/reactivecore/lib/utils/types';
+import { arrayOf, object } from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from '../../utils';
+
+class ErrorBoundary extends Component {
+	state = {
+		error: null,
+	}
+
+	invokeErrorCallback() {
+		const error = this.props.error || this.state.error;
+		if (this.props.onError) {
+			this.props.onError(error, this.props.componentId);
+		}
+	}
+	static getDerivedStateFromError(error) {
+		return ({ error });
+	}
+	componentDidCatch() {
+		this.invokeErrorCallback();
+	}
+	componentDidUpdate() {
+		this.invokeErrorCallback();
+	}
+
+	render() {
+		const error = this.props.error || this.state.error;
+		if (error) {
+			if (this.props.renderError) {
+				const { componentId } = this.props;
+				return this.props.renderError(error, componentId);
+			}
+			// You can render any custom fallback UI
+			return (
+				<div>
+					<h2>Error occured!</h2>
+					<p>{error.message}</p>
+				</div>
+			);
+		}
+
+		return this.props.children;
+	}
+}
+ErrorBoundary.propTypes = {
+	children: types.children,
+	// eslint-disable-next-line react/forbid-prop-types
+	error: object,
+	componentId: types.string,
+	componentIds: arrayOf(types.string),
+	renderError: types.func,
+	onError: types.func,
+};
+const mapStateToProps = (state, ownProps) => {
+	let listOfComponentsToSearch = Object.keys(state.error);
+	if (ownProps.componentIds && ownProps.componentIds.length) {
+		listOfComponentsToSearch = Object.keys(state.error)
+			.filter(componentId => ownProps.componentIds.includes(componentId));
+	}
+	const error = listOfComponentsToSearch
+		.map(componentId => state.error[componentId])
+		// Find the first non-null error
+		.find(e => e);
+	const componentId = listOfComponentsToSearch.find(id => state.error[id]);
+	return { error, componentId };
+};
+
+export default connect(mapStateToProps, null)(ErrorBoundary);
