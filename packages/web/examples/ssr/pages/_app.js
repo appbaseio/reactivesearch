84ca92a7b1bb6448de8a69b96fb6f0cd88412f0d
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-10-04 16:00:47 +0530

    feat(web): upgrade ssr example to React 18

--- packages/web/examples/ssr/pages/_app.js
+++ packages/web/examples/ssr/pages/_app.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './styles/airbnb.css';
+import '../components/index.css';
 
 export default function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
