c8cafad19379c9646e04e17875c72c0ce1c5eb30
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-10-04 11:03:01 +0530

    fix: lint errors

--- packages/web/examples/typescript/src/index.tsx
+++ packages/web/examples/typescript/src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import React from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') || new Element());
 root.render(<App />);
