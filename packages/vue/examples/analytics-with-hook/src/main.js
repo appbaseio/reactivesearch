ce1eb024c28e61aa2505a90ac8d2bb25265bd92c
Shubham Shah <shubhamshahrising@gmail.com>
2022-11-10 10:22:16 +0530

    feat: new example for analytics

---
+++ packages/vue/examples/analytics-with-hook/src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+import { ReactiveBase, ReactiveList, DataSearch } from "@appbaseio/reactivesearch-vue";
+
+Vue.use(ReactiveBase);
+Vue.use(ReactiveList);
+Vue.use(DataSearch);
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
