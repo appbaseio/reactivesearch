800104a06fdcdf2a97178d0738193ec42b5b3770
lohi212 <suvvarilohita@gmail.com>
2022-12-20 16:49:17 +0530

    refactor: index and facets

--- packages/web/examples/ssr/pages/index.js
+++ packages/web/examples/ssr/pages/index.js
@@ -3,92 +3,149 @@ import React, { Component } from 'react';
 import {
 	ReactiveBase,
 	DataSearch,
-	NumberBox,
-	// RangeSlider,
+	MultiList,
+	RangeSlider,
 	ReactiveList,
-	ResultCard,
+	SelectedFilters,
 } from '@appbaseio/reactivesearch';
 import initReactivesearch from '@appbaseio/reactivesearch/lib/server';
 
 const components = {
 	settings: {
-		app: 'airbnb-dev',
-		url: 'https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io',
+		app: 'movies-demo-app',
+		url: 'https://81719ecd9552:e06db001-a6d8-4cc2-bc43-9c15b1c0c987@appbase-demo-ansible-abxiydt-arc.searchbase.io',
 		theme: {
 			colors: {
-				primaryColor: '#FF3A4E',
+				backgroundColor: '#212121',
+				primaryTextColor: '#fff',
+				primaryColor: '#2196F3',
+				titleColor: '#fff',
+				alertColor: '#d9534f',
+				borderColor: '#666',
 			},
 		},
 		enableAppbase: true,
 	},
 	datasearch: {
 		componentId: 'SearchSensor',
-		dataField: ['name', 'name.search'],
-		autosuggest: false,
-		placeholder: 'Search by house names',
+		dataField: ['original_title', 'original_title.search'],
+		autosuggest: true,
+		placeholder: 'Search for movies...',
 		iconPosition: 'left',
 		className: 'search',
 		highlight: true,
 		URLParams: true,
 	},
-	numberbox: {
-		componentId: 'GuestSensor',
-		dataField: 'accommodates',
-		title: 'Guests',
-		defaultValue: 2,
-		labelPosition: 'right',
-		data: {
-			start: 1,
-			end: 16,
+	multiList: {
+		componentId: 'genres-list',
+		dataField: 'genres.keyword',
+
+		react: {
+			and: ['SearchSensor', 'results', 'vote-average'],
+		},
+		innerClass: {
+			label: 'list-item',
+			input: 'list-input',
 		},
 		URLParams: true,
 	},
-	rangeslider: {
-		componentId: 'PriceSensor',
-		dataField: 'price',
-		title: 'Price Range',
+	rangeSlider: {
+		componentId: 'vote-average',
+		dataField: 'vote_average',
 		range: {
-			start: 10,
-			end: 250,
+			start: 0,
+			end: 10,
 		},
 		rangeLabels: {
-			start: '$10',
-			end: '$250',
+			start: '0',
+			end: '10',
 		},
-		defaultValue: {
-			start: 10,
-			end: 50,
+		react: {
+			and: ['SearchSensor', 'results', 'genres-list'],
 		},
-		stepValue: 10,
-		interval: 20,
+		showHistogram: true,
+		URLParams: true,
 	},
 	resultcard: {
 		className: 'right-col',
-		componentId: 'SearchResult',
+		componentId: 'results',
 		dataField: 'name',
 		size: 12,
 		render: ({ data }) => (
-			<ReactiveList.ResultCardsWrapper>
+			<ReactiveList.ResultCardsWrapper style={{ margin: '8px 0 0' }}>
 				{data.map((item) => (
-					<ResultCard href={item.listing_url} key={item._id}>
-						<ResultCard.Image src={item.images ? item.images[0] : ''} />
-						<ResultCard.Title>{item.name}</ResultCard.Title>
-						<ResultCard.Description>
-							<div>
-								<div className="price">${item.price}</div>
-								<p className="info">
-									{item.room_type} · {item.accommodates} guests
-								</p>
-							</div>
-						</ResultCard.Description>
-					</ResultCard>
+					<div style={{ marginRight: '15px' }} className="main-description">
+						<div className="ih-item square effect6 top_to_bottom">
+							<a
+								target="#"
+								href={'https://www.google.com/search?q=' + item.original_title}
+							>
+								<div className="img">
+									<img
+										src={item.poster_path}
+										alt={item.original_title}
+										className="result-image"
+									/>
+								</div>
+								<div className="info colored">
+									<h3 className="overlay-title">{item.original_title}</h3>
+
+									<div className="overlay-description">{item.overview}</div>
+
+									<div className="overlay-info">
+										<div className="rating-time-score-container">
+											<div className="sub-title Rating-data">
+												<b>
+													Ratings
+													<span className="details">
+														{' '}
+														{item.vote_average}
+													</span>
+												</b>
+											</div>
+											<div className="time-data">
+												<b>
+													<span className="time">
+														<i className="fa fa-clock-o" />{' '}
+													</span>{' '}
+													<span className="details">
+														{item.release_date}
+													</span>
+												</b>
+											</div>
+											<div className="sub-title Score-data">
+												<b>
+													Popularity:
+													<span className="details">
+														{' '}
+														{item.popularity}
+													</span>
+												</b>
+											</div>
+										</div>
+										<div className="vote-average-lang-container">
+											<div className="sub-title language-data">
+												<b>
+													Language:
+													<span className="details">
+														{' '}
+														{item.original_language}
+													</span>
+												</b>
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
 				))}
 			</ReactiveList.ResultCardsWrapper>
 		),
 		pagination: true,
 		URLParams: true,
 		react: {
-			and: ['SearchSensor', 'GuestSensor'],
+			and: ['SearchSensor', 'vote-average', 'genres-list'],
 		},
 		innerClass: {
 			resultStats: 'result-stats',
@@ -96,19 +153,6 @@ const components = {
 			listItem: 'list-item',
 			image: 'image',
 		},
-		sortOptions: [
-			{
-				label: 'Bed Type',
-				dataField: 'bed_type',
-				sortBy: 'asc',
-			},
-			{
-				label: 'Bedrooms',
-				dataField: 'bedrooms',
-				sortBy: 'asc',
-			},
-		],
-		defaultSortOption: 'Bedrooms',
 	},
 };
 
@@ -122,13 +166,13 @@ export default class Main extends Component {
 						source: DataSearch,
 					},
 					{
-						...components.numberbox,
-						source: NumberBox,
+						...components.multiList,
+						source: MultiList,
 					},
-					// {
-					// 	...components.rangeslider,
-					// 	source: RangeSlider,
-					// },
+					{
+						...components.rangeSlider,
+						source: RangeSlider,
+					},
 					{
 						...components.resultcard,
 						source: ReactiveList,
@@ -140,21 +184,79 @@ export default class Main extends Component {
 		};
 	}
 
-	render() {
-		return (
-			<div className="container">
-				<ReactiveBase {...components.settings} initialState={this.props.store}>
-					<nav className="nav">
-						<div className="title">Airbeds</div>
-						<DataSearch {...components.datasearch} />
-					</nav>
-					<div className="left-col">
-						<NumberBox {...components.numberbox} />
-						{/* <RangeSlider {...components.rangeslider} /> */}
-					</div>
+	constructor(props) {
+		super(props);
 
-					<ReactiveList {...components.resultcard} />
+		this.state = {
+			isClicked: false,
+			message: '🔬 Show Filters',
+		};
+	}
+
+	handleClick() {
+		this.setState({
+			isClicked: !this.state.isClicked,
+			message: this.state.isClicked ? '🔬 Show Filters' : '🎬 Show Movies',
+		});
+	}
+
+	render() {
+		console.log(components.rangeSlider);
+
+		return (
+			<div className="main-container">
+				<ReactiveBase {...components.settings} initialState={this.props.store}>
+					<div className="navbar">
+						<div className="header-container">🎥 MovieSearch</div>
+
+						<div className="search-container">
+							<DataSearch {...components.datasearch} />
+						</div>
+						<div className="sub-container">
+							<div
+								className={this.state.isClicked ? 'left-bar-optional' : 'left-bar'}
+							>
+								<div className="filter-heading center">
+									<b>
+										{' '}
+										<i className="fa fa-pied-piper-alt" /> Genres{' '}
+									</b>
+								</div>
+
+								<MultiList {...components.multiList} className="genres-filter" />
+								<hr className="blue" />
+
+								<div className="filter-heading center">
+									<b>
+										<i className="fa fa-star" /> Ratings
+									</b>
+								</div>
+								<RangeSlider
+									{...components.rangeSlider}
+									className="review-filter"
+								/>
+							</div>
+
+							<div
+								className={
+									this.state.isClicked
+										? 'result-container-optional'
+										: 'result-container'
+								}
+							>
+								<SelectedFilters
+									showClearAll={true}
+									clearAllLabel="Clear filters"
+									className="selected-filters"
+								/>
+								<ReactiveList {...components.resultcard} />
+							</div>
+						</div>
+					</div>
 				</ReactiveBase>
+				<button className="toggle-button" onClick={this.handleClick.bind(this)}>
+					{this.state.message}
+				</button>
 			</div>
 		);
 	}
