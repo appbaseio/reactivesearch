f8c1a8fc3fa415349646d3d96ec918a73fd5877b
Shubham Shah <shubhamshahrising@gmail.com>
2023-01-18 10:37:28 +0530

    feat: upgrade analytics example

--- packages/vue/examples/analytics-with-hook/src/main.js
+++ packages/vue/examples/analytics-with-hook/src/main.js
@@ -1,12 +1,6 @@
-import Vue from "vue";
-import App from "./App.vue";
-import { ReactiveBase, ReactiveList, DataSearch } from "@appbaseio/reactivesearch-vue";
+import { createApp } from 'vue';
+import App from './App.vue';
 
-Vue.use(ReactiveBase);
-Vue.use(ReactiveList);
-Vue.use(DataSearch);
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
+const app = createApp(App);
+app.config.warnHandler = () => {};
+app.mount('#app'); // Vue Instance - Root component
