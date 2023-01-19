3875a81d2a431fea6fd5b49d3bcfa1ff07ef8063
Shubham Shah <shubhamshahrising@gmail.com>
2023-01-19 11:48:54 +0530

    fix: resultlist components

--- packages/vue/examples/mongo-examples/search-box/src/main.js
+++ packages/vue/examples/mongo-examples/search-box/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
+import VueRs from '@appbaseio/reactivesearch-vue'
 import App from './App.vue';
 
 const app = createApp(App);
 app.config.warnHandler = () => {};
+app.use(VueRs)
 app.mount('#app'); // Vue Instance - Root component
