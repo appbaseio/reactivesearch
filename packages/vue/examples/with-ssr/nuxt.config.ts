4ff7f2abe1f0c589a7334a3e3bf3f04a90c94134
bietkul <kuldepsaxena155@gmail.com>
2023-02-01 14:21:23 +0530

    fix: ssr config

--- packages/vue/examples/with-ssr/nuxt.config.ts
+++ packages/vue/examples/with-ssr/nuxt.config.ts
@@ -1,17 +1,19 @@
 export default defineNuxtConfig({
 	telemetry: false,
+	build: {
+		transpile: ['@appbaseio/reactivesearch-vue']
+	},
 	vite: {
 		optimizeDeps: {
 			include: [
 				'@appbaseio/reactivecore',
 				'@appbaseio/reactivesearch-vue',
-				'vue-emotion',
 				'fast-deep-equal',
 			],
 		},
 		build: {
 			commonjsOptions: {
-				include: [/reactivecore/, /vue-emotion/, /reactivesearch/, /node_modules/],
+				include: [/reactivecore/, /reactivesearch/, /node_modules/],
 			},
 		},
 	},
