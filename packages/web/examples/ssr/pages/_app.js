8e45bb96cc7c07fc8ca4edc4eae1a86e63380d87
Shubham Shah <shubhamshahrising@gmail.com>
2023-02-13 11:21:47 +0530

    chore: relocate folder outside of pages

--- packages/web/examples/ssr/pages/_app.js
+++ packages/web/examples/ssr/pages/_app.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import './styles/movies.css';
+import '../styles/movies.css';
 import '../components/index.css';
 
 export default function MyApp({ Component, pageProps }) {
