de9f96027ccb2d6fa44251bd5bfb92533f7de221
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-03-07 12:08:50 +0530

    feat(web): add showVoiceSearch example for SearchBox

---
+++ packages/web/examples/SearchBoxWithVoiceSearch/src/index.js
@@ -0,0 +1,91 @@
+import ReactDOM from 'react-dom/client';
+
+import { ReactiveBase, SearchBox, ReactiveList, ResultCard } from '@appbaseio/reactivesearch';
+
+import './index.css';
+
+const Main = () => (
+	<ReactiveBase
+		app="movies-demo-app"
+		url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
+	>
+		<div className="row">
+			<div className="col">
+				<SearchBox
+					title="SearchBox"
+					dataField={['original_title', 'original_title.search']}
+					componentId="MoviesSensor"
+					showVoiceSearch
+					autosuggest={false}
+				/>
+			</div>
+
+			<div className="col">
+				<ReactiveList
+					componentId="SearchResult"
+					dataField="original_title"
+					size={10}
+					className="result-list-container"
+					pagination
+					react={{
+						and: 'BookSensor',
+					}}
+					render={({ data }) => (
+						<ReactiveList.ResultCardsWrapper>
+							{data.map((item) => (
+								<ResultCard id={item._id} key={item._id}>
+									<ResultCard.Image src={item.image} />
+									<ResultCard.Title>
+										<div
+											className="book-title"
+											dangerouslySetInnerHTML={{
+												__html: item.original_title,
+											}}
+										/>
+									</ResultCard.Title>
+
+									<ResultCard.Description>
+										<div className="flex column justify-space-between">
+											<div>
+												<div>
+													by{' '}
+													<span className="authors-list">
+														{item.authors}
+													</span>
+												</div>
+												<div className="ratings-list flex align-center">
+													<span className="stars">
+														{
+															/* eslint-disable */
+															Array(item.average_rating_rounded)
+																.fill('x')
+																.map((_, index) => (
+																	<i
+																		className="fas fa-star"
+																		key={index}
+																	/>
+																))
+															/* eslint-enable */
+														}
+													</span>
+													<span className="avg-rating">
+														({item.average_rating} avg)
+													</span>
+												</div>
+											</div>
+											<span className="pub-year">
+												Pub {item.original_publication_year}
+											</span>
+										</div>
+									</ResultCard.Description>
+								</ResultCard>
+							))}
+						</ReactiveList.ResultCardsWrapper>
+					)}
+				/>
+			</div>
+		</div>
+	</ReactiveBase>
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />);
