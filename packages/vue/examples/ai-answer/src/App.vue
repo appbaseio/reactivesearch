959639c79bc985d240ad5549f8a4486dfc1d373a
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-04-11 12:48:27 +0530

    vue: update example

--- packages/vue/examples/ai-answer/src/App.vue
+++ packages/vue/examples/ai-answer/src/App.vue
@@ -3,6 +3,7 @@
 		<reactive-base
 			app="good-books-ds"
 			url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
+			themePreset="light"
 		>
 			<search-box
 				className="result-list-container"
@@ -25,7 +26,33 @@
 				componentId="AIComponent"
 				:react="{ and: ['BookSensor'] }"
 				title="AI Chat Box"
-			/>
+			>
+				<!-- <template v-slot:render="{ loading, data, error }">
+					<div v-if="loading">loading...</div>
+					<pre v-else-if="error">{{ JSON.stringify(error) }}</pre>
+					<div
+						v-else-if="data && Array.isArray(data)"
+						style="width: 80%; margin: 0 auto; padding: 20px"
+					>
+						<div
+							v-for="(message, index) in data"
+							:key="index"
+							:style="{
+								display: 'flex',
+								justifyContent: message.role === 'user' ? 'flex-end' : 'flex-start',
+							}"
+						>
+							<div :style="getMessageStyle(message)">
+								{{ message.content }}
+							</div>
+						</div>
+					</div>
+				</template>
+				<template v-slot:renderError="errorParam, handleRetry">
+					<div>{{ JSON.stringify(errorParam) }}</div>
+					<button v-on:click="handleRetry">fff</button>
+				</template> -->
+			</a-i-answer>
 			<reactive-list
 				componentId="SearchResult"
 				dataField="original_title.keyword"
@@ -78,6 +105,24 @@ import { ReactiveBase, ReactiveList, SearchBox, AIAnswer } from '@appbaseio/reac
 export default {
 	name: 'app',
 	components: { ReactiveBase, ReactiveList, SearchBox, AIAnswer },
+	methods: {
+		getMessageStyle(message) {
+			const isSender = message.role === 'user';
+			return {
+				backgroundColor: isSender ? '#cce5ff' : '#f8f9fa',
+				padding: '10px',
+				borderRadius: '7px',
+				marginBottom: '10px',
+				maxWidth: '80%',
+				alignSelf: isSender ? 'flex-end' : 'flex-start',
+				display: 'inline-block',
+				border: '1px solid',
+				color: isSender ? '#004085' : '#383d41',
+				position: 'relative',
+				whiteSpace: 'pre-wrap',
+			};
+		},
+	},
 };
 </script>
 
