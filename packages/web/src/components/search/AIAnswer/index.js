08326e5fe210579c0d8c5f2e6c61c8b5dca11eb3
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-04-12 12:07:32 +0530

    refactor(web): remove log statement

--- packages/web/src/components/search/AIAnswer/index.js
+++ packages/web/src/components/search/AIAnswer/index.js
@@ -41,7 +41,6 @@ const AIAnswer = (props) => {
 		}
 	};
 	useEffect(() => {
-		console.log('props.AIResponseError', props.AIResponseError);
 		setErrorState(props.AIResponseError);
 	}, [props.AIResponseError]);
 
