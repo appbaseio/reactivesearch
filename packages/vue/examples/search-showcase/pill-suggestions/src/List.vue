5ed26a0f5a987727881035890a5209679159e673
Shubham Shah <shubhamshahrising@gmail.com>
2023-05-03 18:42:29 +0530

    feat: pill based suggestions

---
+++ packages/vue/examples/search-showcase/pill-suggestions/src/List.vue
@@ -0,0 +1,33 @@
+<template>
+
+<ul v-if="isOpen">
+						<li
+							:style="{ backgroundColor: this.highlightedIndex ? 'grey' : 'transparent' }"
+							v-for="suggestion in (data || []).map(s =>  ({
+								label: s.source.authors,
+								value: s.source.authors,
+								key: s._id,
+							}))"
+							v-bind="getItemProps({ item: suggestion })"
+							v-on="getItemEvents({ item: suggestion })"
+							:key="suggestion._id"
+						>
+							{{ suggestion.label }}
+						</li>
+					</ul>
+</template>
+
+<script>
+export default {
+	name: 'app',
+	computed(){
+        return {
+            suggestions: (this.data || []).map(s =>  ({
+								label: s.source.authors,
+								value: s.source.authors,
+								key: s._id,
+							})),
+        }
+    }
+};
+</script>
