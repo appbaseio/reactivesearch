9439c5b8ef8173498cbb0cc523dfe4d6bc5f46b2
Shubham Shah <shubhamshahrising@gmail.com>
2023-05-11 14:36:17 +0530

    feat: add custom render method

--- packages/vue/examples/search-showcase/searchbox-inline-ai-response/src/App.vue
+++ packages/vue/examples/search-showcase/searchbox-inline-ai-response/src/App.vue
@@ -21,7 +21,50 @@
 				}"
 				:autosuggest="true"
 				:enableAI="true"
-			/>
+			>
+				<template
+					#render="{
+							downshiftProps: {
+									isOpen,
+									getItemProps,
+									highlightedIndex,
+									selectedItem,
+								},
+							AIData: { answer: aiAnswer, showAIScreen },
+							data
+					}"
+				>
+						<div v-if="isOpen">
+							<div v-if="showAIScreen" class="suggestions">
+								<div
+									:style="{
+										alignSelf: 'flex-start',
+										margin: 8,
+										maxWidth: '70%',
+									}"
+									>
+									<div
+										:style="{
+										display: 'inline-block',
+										maxWidth: '100%',
+										backgroundColor: '#f1f1f1',
+										color: 'black',
+										borderRadius: '16px',
+										padding: '8px 16px',
+										whiteSpace: 'pre-wrap',
+										wordWrap: 'break-word',
+										}"
+									>
+										{{aiAnswer || "Loading..."}}
+									</div>
+								</div>
+							</div>
+							<div v-else>
+								No suggestions
+							</div>
+						</div>
+				</template>
+			</search-box>
 		</reactive-base>
 	</div>
 </template>
@@ -64,5 +107,118 @@ export default {
 .ai-answer-container{
 	display: block;
     margin: 40px;
+}
+.spinner {
+  position: absolute;
+  height: 80vh;
+  width: 100vw;
+  left: 0px;
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
+  z-index: 1;
+  background-color: white;
+}
+.headingTag {
+  background: linear-gradient(
+    30deg,
+    rgb(59, 130, 246) 0%,
+    rgb(59, 130, 246) 0%,
+    rgb(255, 42, 111) 100%
+  );
+  margin-right: auto;
+  border-radius: var(--bs-border-radius);
+  padding: 5px 10px;
+}
+.suggestions {
+  position: absolute;
+  z-index: 2;
+  box-shadow: #9597a1 0px 8px 24px;
+  background: white;
+  width: 100%;
+  border-radius: 0px 0px 10px 10px;
+  max-height: 500px;
+  overflow: auto;
+  font-size: 0.9rem;
+}
+.suggestionHeading {
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+.suggestion {
+  background-color: white;
+  color: var(--bs-black);
+  font-weight: normal;
+  padding: 10px 15px;
+  width: 100%;
+  height: 100%;
+  display: block;
+  text-decoration: none;
+  position: relative;
+}
+.activeSuggestion,
+.activeSuggestion:hover {
+  background-color: var(--bs-primary);
+  color: white;
+  padding: 10px 15px;
+  width: 100%;
+  height: 100%;
+  display: block;
+  text-decoration: none;
+  position: relative;
+}
+.suggestionTitle {
+  text-transform: uppercase;
+  font-weight: bold;
+  width: 50%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.suggestionBreadcrumb {
+  padding: 0.25rem;
+  margin-top: 0.25rem;
+  font-size: 0.8rem;
+  position: absolute;
+  top: 2px;
+  max-width: 30%;
+  right: 10px;
+  background-color: #eee;
+  color: #6c757d;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  border-radius: var(--bs-border-radius);
+}
+.suggestionDescription {
+  margin-top: 0.25rem;
+  width: 100%;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+}
+@media (max-width: 500px) {
+  .suggestionDescription {
+    -webkit-line-clamp: 3;
+  }
+  .suggestionBreadcrumb {
+    position: relative;
+    top: 0;
+    right: 0;
+    font-size: 0.7rem;
+    display: block;
+    max-width: 100%;
+  }
+  .suggestionTitle {
+    width: 100%;
+  }
+}
+.suggestionIcon {
+  max-width: 3rem;
+  margin: auto;
+}
+.selectedSuggestion {
+  font-weight: bold;
 }
 </style>
