d25858b89f69895fac15c54c0e5cc72fa468be11
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-05-16 08:17:24 +0530

    fix(vue): fix prop usage and styling

--- packages/vue/examples/search-showcase/searchbox-inline-ai-response/src/App.vue
+++ packages/vue/examples/search-showcase/searchbox-inline-ai-response/src/App.vue
@@ -35,7 +35,7 @@
         :size="5"
         :urlparams="true"
         :autosuggest="true"
-        :enable-ai="true"
+        :enable-a-i="true"
         :aiconfig="{
           docTemplate:
             'title is \'${source.title}\', page content is \'${source.tokens}\', URL is https://docs.reactivesearch.io${source.url}',
@@ -49,19 +49,15 @@
       >
         <template
           #render="{
-							downshiftProps: {
-									isOpen,
-									getItemProps,
-									highlightedIndex,
-									selectedItem,
-								},
-							AIData: { answer: aiAnswer, showAIScreen },
-							data
+						downshiftProps: { isOpen, getItemProps, highlightedIndex, selectedItem },
+						AIData: { answer: aiAnswer, showAIScreen, isAILoading, AIError },
+						data,
 					}"
         >
           <div v-if="isOpen">
             <div class="suggestions">
-              <div v-if="showAIScreen" >
+              <div v-if="showAIScreen">
+                🚨🚨🚨🚨🚨🚨🚨🚨{{ isAILoading }}{{ AIError }}
                 <div
                   :style="{
                     alignSelf: 'flex-start',
@@ -81,32 +77,27 @@
                       wordWrap: 'break-word',
                     }"
                   >
-                    {{ aiAnswer || "Loading..." }}
+                    {{ aiAnswer || 'Loading...' }}
                   </div>
                 </div>
               </div>
               <div v-else-if="!(data && data.length)">
-                <p
-                  class="bg-gray p-2 m-0 suggestionHeading"
-                >
+                <p class="bg-gray p-2 m-0 suggestionHeading">
                   Frequently Asked Questions
-                  <span
-                    role="img"
-                    aria-label="confused">
-                    🤔
-                  </span>
+                  <span 
+                    role="img" 
+                    aria-label="confused"> 🤔 </span>
                 </p>
                 <div>
                   <div
                     v-for="(item, index) in faqs"
                     :key="item.id + index"
-                    v-bind="getItemProps({item})"
+                    v-bind="getItemProps({ item })"
                     :class="{
                       activeSuggestion: highlightedIndex === index,
                       suggestion: true,
                       selectedSuggestion:
-                        selectedItem &&
-                        selectedItem.value === item.value
+                        selectedItem && selectedItem.value === item.value,
                     }"
                   >
                     <span className="clipText">{{ item.value }}</span>
@@ -114,40 +105,32 @@
                 </div>
               </div>
               <div v-else-if="data && data.length">
-                <p
-                  class="bg-gray p-2 m-0 suggestionHeading"
-                >
+                <p class="bg-gray p-2 m-0 suggestionHeading">
                   Documentation pages
-                  <span
-                    role="img"
-                    aria-label="confused">
-                    📄
-                  </span>
+                  <span 
+                    role="img" 
+                    aria-label="confused"> 📄 </span>
                 </p>
                 <div>
                   <a
                     v-for="(item, index) in data"
-                    :key="item._id +index"
-                    v-bind="getItemProps({item})"
+                    :key="item._id + index"
+                    v-bind="getItemProps({ item })"
                     :class="{
                       activeSuggestion: highlightedIndex === index,
                       suggestion: true,
                       selectedSuggestion:
-                        selectedItem &&
-                        selectedItem.value === item.value
+                        selectedItem && selectedItem.value === item.value,
                     }"
                     :href="`https://docs.reactivesearch.io${item._source.url}`"
                     target="_blank"
                     rel="noreferrer"
                   >
                     <div class="row">
-                      <div class="d-flex justify-content-center align-items-center col col-3 col-md-1">
-                        <div
-                          class="p-1
-                          bg-white
-                          rounded
-                          suggestionIcon"
-                        >
+                      <div
+                        class="d-flex justify-content-center align-items-center col col-3 col-md-1"
+                      >
+                        <div class="p-1 bg-white rounded suggestionIcon">
                           <img
                             :src="getIcon(item._source.keywords)"
                             class="w-100 h-100"
@@ -156,25 +139,31 @@
                         </div>
                       </div>
                       <div class="col col-9 col-md-11">
-                        <div
-                          :title="item.value"
-                          class="suggestionTitle"
-                        >
+                        <div 
+                          :title="item.value" 
+                          class="suggestionTitle">
                           {{ item.value || item._source.title }}
                         </div>
                         <div
-                          v-if="item._source.heading
-                            ? `${item._source.meta_title} > ${item._source.heading}`
-                          : item._source.meta_title">
-                          <span
-                            :title="item._source.heading
+                          v-if="
+                            item._source.heading
                               ? `${item._source.meta_title} > ${item._source.heading}`
-                            : item._source.meta_title"
+                              : item._source.meta_title
+                          "
+                        >
+                          <span
+                            :title="
+                              item._source.heading
+                                ? `${item._source.meta_title} > ${item._source.heading}`
+                                : item._source.meta_title
+                            "
                             class="suggestionBreadcrumb"
                           >
-                            {{ item._source.heading
-                              ? `${item._source.meta_title} > ${item._source.heading}`
-                            : item._source.meta_title }}
+                            {{
+                              item._source.heading
+                                ? `${item._source.meta_title} > ${item._source.heading}`
+                                : item._source.meta_title
+                            }}
                           </span>
                         </div>
                         <div
@@ -200,7 +189,7 @@
 
 <script>
 import { ReactiveBase, ReactiveList, SearchBox, AIAnswer } from '@appbaseio/reactivesearch-vue';
-import {getIcon} from './getIcon'
+import { getIcon } from './getIcon';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
@@ -225,7 +214,7 @@ export default {
 				whiteSpace: 'pre-wrap',
 			};
 		},
-		getIcon
+		getIcon,
 	},
 };
 </script>
@@ -237,121 +226,120 @@ export default {
 	-moz-osx-font-smoothing: grayscale;
 	color: #2c3e50;
 }
-.ai-answer-container{
+.ai-answer-container {
 	display: block;
-    margin: 40px;
+	margin: 40px;
 }
 .spinner {
-  position: absolute;
-  height: 80vh;
-  width: 100vw;
-  left: 0px;
-  display: flex;
-  justify-content: center;
-  padding-top: 50px;
-  z-index: 1;
-  background-color: white;
+	position: absolute;
+	height: 80vh;
+	width: 100vw;
+	left: 0px;
+	display: flex;
+	justify-content: center;
+	padding-top: 50px;
+	z-index: 1;
+	background-color: white;
 }
 .headingTag {
-  background: linear-gradient(
-    30deg,
-    rgb(59, 130, 246) 0%,
-    rgb(59, 130, 246) 0%,
-    rgb(255, 42, 111) 100%
-  );
-  margin-right: auto;
-  border-radius: var(--bs-border-radius);
-  padding: 5px 10px;
+	background: linear-gradient(
+		30deg,
+		rgb(59, 130, 246) 0%,
+		rgb(59, 130, 246) 0%,
+		rgb(255, 42, 111) 100%
+	);
+	margin-right: auto;
+	border-radius: var(--bs-border-radius);
+	padding: 5px 10px;
 }
 .suggestions {
-  position: absolute;
-  z-index: 2;
-  box-shadow: #9597a1 0px 8px 24px;
-  background: white;
-  width: 100%;
-  border-radius: 0px 0px 10px 10px;
-  max-height: 500px;
-  overflow: auto;
-  font-size: 0.9rem;
+	z-index: 2;
+	box-shadow: #9597a1 0px 8px 24px;
+	background: white;
+	width: 100%;
+	border-radius: 0px 0px 10px 10px;
+	max-height: 500px;
+	overflow: auto;
+	font-size: 0.9rem;
 }
 .suggestionHeading {
-  font-size: 0.8rem;
-  font-weight: bold;
+	font-size: 0.8rem;
+	font-weight: bold;
 }
 .suggestion {
-  background-color: white;
-  color: var(--bs-black);
-  font-weight: normal;
-  padding: 10px 15px;
-  width: 100%;
-  height: 100%;
-  display: block;
-  text-decoration: none;
-  position: relative;
+	background-color: white;
+	color: var(--bs-black);
+	font-weight: normal;
+	padding: 10px 15px;
+	width: 100%;
+	height: 100%;
+	display: block;
+	text-decoration: none;
+	position: relative;
 }
 .activeSuggestion,
 .activeSuggestion:hover {
-  background-color: var(--bs-primary);
-  color: white;
-  padding: 10px 15px;
-  width: 100%;
-  height: 100%;
-  display: block;
-  text-decoration: none;
-  position: relative;
+	background-color: var(--bs-primary);
+	color: white;
+	padding: 10px 15px;
+	width: 100%;
+	height: 100%;
+	display: block;
+	text-decoration: none;
+	position: relative;
 }
 .suggestionTitle {
-  text-transform: uppercase;
-  font-weight: bold;
-  width: 50%;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
+	text-transform: uppercase;
+	font-weight: bold;
+	width: 50%;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
 }
 .suggestionBreadcrumb {
-  padding: 0.25rem;
-  margin-top: 0.25rem;
-  font-size: 0.8rem;
-  position: absolute;
-  top: 2px;
-  max-width: 30%;
-  right: 10px;
-  background-color: #eee;
-  color: #6c757d;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  border-radius: var(--bs-border-radius);
+	padding: 0.25rem;
+	margin-top: 0.25rem;
+	font-size: 0.8rem;
+	position: absolute;
+	top: 2px;
+	max-width: 30%;
+	right: 10px;
+	background-color: #eee;
+	color: #6c757d;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	border-radius: var(--bs-border-radius);
 }
 .suggestionDescription {
-  margin-top: 0.25rem;
-  width: 100%;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 2;
-  overflow: hidden;
+	margin-top: 0.25rem;
+	width: 100%;
+	display: -webkit-box;
+	-webkit-box-orient: vertical;
+	-webkit-line-clamp: 2;
+	overflow: hidden;
 }
 @media (max-width: 500px) {
-  .suggestionDescription {
-    -webkit-line-clamp: 3;
-  }
-  .suggestionBreadcrumb {
-    position: relative;
-    top: 0;
-    right: 0;
-    font-size: 0.7rem;
-    display: block;
-    max-width: 100%;
-  }
-  .suggestionTitle {
-    width: 100%;
-  }
+	.suggestionDescription {
+		-webkit-line-clamp: 3;
+	}
+	.suggestionBreadcrumb {
+		position: relative;
+		top: 0;
+		right: 0;
+		font-size: 0.7rem;
+		display: block;
+		max-width: 100%;
+	}
+	.suggestionTitle {
+		width: 100%;
+	}
 }
 .suggestionIcon {
-  max-width: 3rem;
-  margin: auto;
+	max-width: 3rem;
+	margin: auto;
 }
 .selectedSuggestion {
-  font-weight: bold;
+	font-weight: bold;
 }
 </style>
