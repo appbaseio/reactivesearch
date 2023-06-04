a17942ff0390fe35df05cdf20860f7d802cfaa13
Shubham Shah <shubhamshahrising@gmail.com>
2023-06-04 22:45:08 +0530

    feat: add types for reactivecomponent

--- packages/vue/src/components/basic/ReactiveComponentPrivate.d.ts
+++ packages/vue/src/components/basic/ReactiveComponentPrivate.d.ts
@@ -1,3 +1,9 @@
+import {ReactiveComponentProps} from './ReactiveComponent'
+
 declare function ReactiveComponentPrivate(props: any): JSX.Element;
+
+interface ReactiveComponentPrivateProps extends ReactiveComponentProps{
+    componentType: "SEARCHBOX" | "REACTIVELIST" | "MULTILIST" | "MULTIDROPDOWNLIST" | "SINGLEDROPDOWNLIST" | "SINGLELIST" | "RANGESLIDER" | "RANGEINPUT" | "DYNAMICRANGESLIDER"
+}
 
 export default ReactiveComponentPrivate;
