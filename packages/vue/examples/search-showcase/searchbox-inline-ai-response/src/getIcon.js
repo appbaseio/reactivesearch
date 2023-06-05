25a42ee9a473a41adec496c1d756f7db4b320296
Shubham Shah <shubhamshahrising@gmail.com>
2023-06-05 14:15:37 +0530

    fix: codesandbox cloud template issue

--- packages/vue/examples/search-showcase/searchbox-inline-ai-response/src/getIcon.js
+++ packages/vue/examples/search-showcase/searchbox-inline-ai-response/src/getIcon.js
@@ -1,13 +1,12 @@
-// eslint-disable-next-line import/prefer-default-export
 export function getIcon(keywords = []) {
 	if (keywords.includes('vue')) {
-	  return '/vue.svg';
+		return '/vue.svg';
 	}
 	if (keywords.includes('flutter')) {
-	  return '/flutter.svg';
+		return '/flutter.svg';
 	}
 	if (keywords.includes('react')) {
-	  return '/react.svg';
+		return '/react.svg';
 	}
 	return '/block.svg';
 }
