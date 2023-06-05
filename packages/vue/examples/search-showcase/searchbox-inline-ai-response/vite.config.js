dc4c88e2fb4f8e1c82b7d895cb9c4a3b728a5870
Shubham Shah <shubhamshahrising@gmail.com>
2023-06-05 15:13:03 +0530

    fix: codesandbox issue

--- packages/vue/examples/search-showcase/searchbox-inline-ai-response/vite.config.js
+++ packages/vue/examples/search-showcase/searchbox-inline-ai-response/vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   optimizeDeps: {
     include: [
-      '@appbaseio/reactivecore',
-      '@appbaseio/reactivesearch-vue',
-      'fast-deep-equal',
+      "@appbaseio/reactivecore",
+      "@appbaseio/reactivesearch-vue",
+      "fast-deep-equal",
     ],
   },
   build: {
@@ -16,4 +24,4 @@ export default defineConfig({
       include: [/reactivecore/, /reactivesearch/, /node_modules/],
     },
   },
-})
+});
