02e9cbd5b70da8e960a362cdc56713263fdcf19b
Shubham Shah <shubhamshahrising@gmail.com>
2023-06-29 12:55:47 +0530

    feat: update examples as requested

--- packages/vue/examples/search-showcase/ai-answer-simple/src/App.vue
+++ packages/vue/examples/search-showcase/ai-answer-simple/src/App.vue
@@ -27,8 +27,51 @@
 					componentId="AIComponent"
 					:react="{ and: ['BookSensor'] }"
 					title="AI Chat Box"
+					:showInput="false"
 				></a-i-answer>
 			</div>
+
+			<reactive-list
+				componentId="SearchResult"
+				dataField="original_title.keyword"
+				className="result-list-container"
+				:pagination="true"
+				:size="5"
+				:react="{ and: ['BookSensor'] }"
+			>
+				<template #renderItem="{ item }">
+					<div :id="item._id" class="flex book-content" :key="item._id">
+						<img :src="item.image" alt="Book Cover" class="book-image" />
+						<div class="flex column justify-center ml20">
+							<div class="book-header">{{ item.original_title }}</div>
+							<div class="flex column justify-space-between">
+								<div>
+									<div>
+										by <span class="authors-list">{{ item.authors }}</span>
+									</div>
+									<div class="ratings-list flex align-center">
+										<span class="stars">
+											<i
+												v-for="(item, index) in Array(
+													item.average_rating_rounded,
+												).fill('x')"
+												class="fas fa-star"
+												:key="index"
+											/>
+										</span>
+										<span class="avg-rating"
+											>({{ item.average_rating }} avg)</span
+										>
+									</div>
+								</div>
+								<span class="pub-year"
+									>Pub {{ item.original_publication_year }}</span
+								>
+							</div>
+						</div>
+					</div>
+				</template>
+			</reactive-list>
 		</reactive-base>
 	</div>
 </template>
--- packages/vue/examples/search-showcase/focus-shortcuts/src/App.vue
+++ packages/vue/examples/search-showcase/focus-shortcuts/src/App.vue
@@ -10,14 +10,6 @@
 				:dataField="['original_title', 'original_title.search']"
 				:URLParams="true"
 				:size="10"
-				:enablePopularSuggestions="true"
-				:popularSuggestionsConfig="{ size: 3, minChars: 2, index: 'good-books-ds' }"
-				:enableRecentSuggestions="true"
-				:recentSuggestionsConfig="{
-					size: 3,
-					index: 'good-books-ds',
-					minChars: 4,
-				}"
 				:showVoiceSearch="true"
 				:autosuggest="true"
 				:focusShortcuts="['⌘ + K']"
