019ec0055a8fcc9841bc9ece53a3da02b5aaf6a1
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-07-05 08:23:32 +0530

    fix(web): typing flicker and auto scroll issue

--- packages/web/src/components/search/SearchBox.js
+++ packages/web/src/components/search/SearchBox.js
@@ -1625,6 +1625,7 @@ const SearchBox = (props) => {
 																				}}
 																				showTypingEffect={
 																					showTypingEffect
+																				&& !isAITyping
 																				}
 																			/>
 																		</Answer>
