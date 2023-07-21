b9c5a35f4a316193d53a8e5f9b1f75995e9f4b11
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-07-21 10:03:19 +0530

    vue: post release 1.34.1

--- packages/vue/examples/reactive-google-map/src/App.vue
+++ packages/vue/examples/reactive-google-map/src/App.vue
@@ -4,6 +4,7 @@
 			app="earthquakes"
 			url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
 			:enable-appbase="true"
+			:httpRequestTimeout="0.2"
 		>
 			<div class="row">
 				<div class="col">
