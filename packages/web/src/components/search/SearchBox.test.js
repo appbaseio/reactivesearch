28159083b73650cdce6da8ee06ec2a5f4a3efb2c
Shubham Shah <shubhamshahrising@gmail.com>
2023-08-28 16:16:49 +0530

    test: render custom source documents

--- packages/web/src/components/search/SearchBox.test.js
+++ packages/web/src/components/search/SearchBox.test.js
@@ -591,3 +591,34 @@ it('should render AI response & showSourceDocuments', async () => {
 
 	expect(elem.toJSON()).toMatchSnapshot();
 });
+
+it('should render AI response & custom source documents', async () => {
+	const elem = renderer
+		.create(
+			<ReactiveBase app="test" url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@localhost:800">
+				<SearchBox
+					testMode
+					componentId="MockSearchBox"
+					dataField="original_title"
+					mockData={{
+						hits: AI.MOCK_HITS_DATA,
+						AI_RESPONSE: AI.MOCK_AI_RESPONSE,
+						rawData: AI.MOCK_RAW_DATA,
+					}}
+					AIUIConfig={{
+						showSourceDocuments: true,
+						renderSourceDocument: obj => <span role="img" aria-label="img">❤️ {obj.original_title}</span>,
+					}}
+					enableAI
+					isOpen
+				/>
+			</ReactiveBase>,
+		);
+
+	// Wait for all updates to complete
+	await act(async () => {
+		await new Promise(resolve => setTimeout(resolve, 0));
+	});
+
+	expect(elem.toJSON()).toMatchSnapshot();
+});
