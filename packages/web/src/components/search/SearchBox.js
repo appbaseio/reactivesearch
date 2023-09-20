db8fc4f48aea6f5a5810260e30789c5a0801caa9
Shubham Shah <shubhamshahrising@gmail.com>
2023-09-20 13:47:01 +0530

    feat: abilitiy to customize icons

--- packages/web/src/components/search/SearchBox.js
+++ packages/web/src/components/search/SearchBox.js
@@ -92,17 +92,33 @@ md.set({
 
 const { useConstructor } = HOOKS;
 
-const CameraIcon = ({ onClick, title }) => (
-	<IconWrapper onClick={onClick} title={title}>
+const CameraIcon = ({
+	onClick, title, icon, iconURL,
+}) => {
+	const defaultIcon = (
 		<svg fill="currentColor" width="30px" height="30px" viewBox="0 -2 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" >
 			<g id="Page-1" stroke="none" strokeWidth="1" fillRule="evenodd" >
 				<g id="Icon-Set-Filled" transform="translate(-258.000000, -467.000000)" >
 					<path d="M286,471 L283,471 L282,469 C281.411,467.837 281.104,467 280,467 L268,467 C266.896,467 266.53,467.954 266,469 L265,471 L262,471 C259.791,471 258,472.791 258,475 L258,491 C258,493.209 259.791,495 262,495 L286,495 C288.209,495 290,493.209 290,491 L290,475 C290,472.791 288.209,471 286,471 Z M274,491 C269.582,491 266,487.418 266,483 C266,478.582 269.582,475 274,475 C278.418,475 282,478.582 282,483 C282,487.418 278.418,491 274,491 Z M274,477 C270.687,477 268,479.687 268,483 C268,486.313 270.687,489 274,489 C277.313,489 280,486.313 280,483 C280,479.687 277.313,477 274,477 L274,477 Z" id="camera" />
 				</g>
 			</g>
-		</svg>
-	</IconWrapper>
-);
+		</svg>);
+	const ImageSearchIcon = icon || defaultIcon;
+	const ImageSearchURLIcon = iconURL
+		?	(
+			<img
+				src={iconURL}
+				width="30px"
+				alt="icon"
+			/>
+		) : null;
+
+	return (
+		<IconWrapper onClick={onClick} title={title}>
+			{ImageSearchURLIcon || ImageSearchIcon}
+		</IconWrapper>
+	);
+};
 
 CameraIcon.propTypes = {
 	fill: string,
@@ -1197,13 +1213,14 @@ const SearchBox = (props) => {
 			onClick={e =>
 				handleShowImageDropdown(e, !showImageDropdown)}
 		/>
-	) : (
-		<CameraIcon
-			title={props.imageSearchTooltip || 'Search by image'}
-			onClick={(e) => {
-				handleShowImageDropdown(e, !showImageDropdown);
-			}}
-		/>);
+	) : (<CameraIcon
+		title={props.imageSearchTooltip || 'Search by image'}
+		onClick={(e) => {
+			handleShowImageDropdown(e, !showImageDropdown);
+		}}
+		icon={props.imageSearchIcon}
+		iconURL={props.imageSearchIconURL}
+	/>);
 
 	const renderIcons = () => {
 		const {
