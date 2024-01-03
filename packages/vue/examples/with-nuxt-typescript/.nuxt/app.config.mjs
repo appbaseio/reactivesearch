066b88e39d41133c4e63f2cadb8bdb6675697cfb
Siddharth Kothari <sids.aquarius@gmail.com>
2024-01-04 00:45:52 +0530

    chore: update examples to new lib version

---
+++ packages/vue/examples/with-nuxt-typescript/.nuxt/app.config.mjs
@@ -0,0 +1,20 @@
+
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
+
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "d36fc3fc-868f-462a-b7af-cc6d9457242a"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
