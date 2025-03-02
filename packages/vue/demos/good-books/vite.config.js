c5074474ef0937d6db0f31707613e97bb6378273
Siddharth Kothari <sids.aquarius@gmail.com>
2025-03-02 21:46:43 +0530

    chore (vue): use vite instead of vue/cli-service

---
+++ packages/vue/demos/good-books/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
